5a093815025aa88f01e7ce441e77d505889c6b05
Nicolai Davies <nicolaidavies.nd@gmail.com>
2016-12-27 17:20:33 +0100

    Add profile page

---
+++ src/pages/meta/user/profile.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class='profile'>
+
+    <md-card>
+      <md-card-content>
+        <md-subheader>Name</md-subheader>
+        <p class="profile-text">{{name}}</p>
+        <md-subheader>Email</md-subheader>
+        <p class="profile-text">{{email}}</p>
+
+        <md-button class="md-raised md-accent" @click="logout">Logout</md-button>
+      </md-card-content>
+    </md-card>
+
+  </div>
+</template>
+
+<script>
+import firebase from 'firebase'
+
+export default {
+  mounted() {
+    const user = firebase.auth().currentUser
+    this.email = user.email
+    this.name = user.displayName
+  },
+  data() {
+    return {
+      name: '',
+      email: '',
+    }
+  },
+  methods: {
+    logout() {
+      this.$router.push({name: 'meta:logout'})
+    }
+  }
+}
+</script>
+
+<style>
+  .profile {
+    max-width: 500px;
+    margin: 1em auto;
+    padding: 0 0.5em;
+  }
+
+  .profile-text {
+    padding-left: 16px;
+  }
+
+</style>
