1bae7899c60bfc3f4e813d9ed92f7d76aee81189
Nicolai Davies <nicolaidavies.nd@gmail.com>
2016-12-29 13:16:15 +0100

    Add accuracy in metres to map

--- src/pages/gps/root.vue
+++ src/pages/gps/root.vue
@@ -7,6 +7,9 @@
 
   export default {
     mounted() {
+
+      // Read more: https://www.w3.org/TR/2016/REC-geolocation-API-20161108/
+
       this.lmap = Leaflet.map('gps-example', {tms: true})
 
       const url = 'https://api.mapbox.com/styles/v1/onlyjsmith/civ9t5x7e001y2imopb8c7p52/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoib25seWpzbWl0aCIsImEiOiI3R0ZLVGtvIn0.jBTrIysdeJpFhe8s1M_JgA'
@@ -14,9 +17,17 @@
       Leaflet.tileLayer(url).addTo(this.lmap)
 
       if ('geolocation' in navigator) {
+        // watchPosition() also emits a location initially, no need for getCurrentPosition()
         this.watchID = navigator.geolocation.watchPosition((position) => {
-          this.onLocationFound({latlng: L.latLng(position.coords.latitude, position.coords.longitude)})
-        });  
+          let {accuracy} = position.coords // accuracy is in m
+          this.onLocationFound({latlng: L.latLng(position.coords.latitude, position.coords.longitude), accuracy})
+        }, e => console.log(e), {enableHighAccuracy: true});  // can disable highAccuracy for better performance, maybe check this dynamically somehow?
+
+        this.accuracyLabel = L.control.attribution({
+          prefix: ``,
+          position: 'topright'
+        }).addTo(this.lmap)
+
       } else {
         alert('GPS is not supported ')
       }
@@ -31,17 +42,18 @@
       return {
         lmap: null,
         watchID: null,
-        marker: null
+        marker: null,
+        accuracyLabel: null
       }
     },
     methods: {
-      onLocationFound({latlng}) {
+      onLocationFound({latlng, accuracy}) {
+        this.accuracyLabel.setPrefix(`<p style="font-size:20px;margin:0;">Accuracy: ${accuracy}m</p>`)
+        this.lmap.setView(latlng, 17)
         if (this.marker) {
           this.marker.setLatLng(latlng);
-          this.lmap.setView(latlng, 17)
         } else {
           this.marker = Leaflet.marker(latlng).addTo(this.lmap)
-          this.lmap.setView(latlng, 17)
         }
       },
       onLocationError(e) {
