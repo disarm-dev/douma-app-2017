1e03264e6ef8c787938af6aa8fd2a09e79776dbc
Jonathan Smith <jonathan@peoplesized.com>
2017-01-31 10:55:32 +0200

    added multiselect

--- src/apps/irs/tasks/tasks.vue
+++ src/apps/irs/tasks/tasks.vue
@@ -3,30 +3,17 @@
     <template v-if='tasksCount <= 0'>
       <div class='md-title'>Load IRS tasks</div>
       <p>Please select a Scale and then an Area of Interest: </p>
-
-      <md-input-container>
-        <label for="spatialScale">Spatial scale</label>
-        <md-select name="spatialScale" v-model="spatialScale">
-          <md-option value="Region">Region</md-option>
-          <md-option value="Locality">Locality</md-option>
-          <md-option value="Inkhundla">Inkhundla</md-option>
-          <md-option value="Village">Village</md-option>
-          <md-option value="Cluster">Cluster</md-option>
-          <md-option value="Homestead">Homestead</md-option>
-          <md-option value="Household">Household</md-option>
-          <md-option value="Structure">Structure</md-option>
-          <md-option value="Room">Room</md-option>
-        </md-select>
-      </md-input-container>
-      <md-input-container>
-        <label for="region">Region</label>
-        <md-select name="region" v-model="region">
-          <md-option value="hhohho">Hhohho</md-option>
-          <md-option value="lubombo">Lubombo</md-option>
-          <md-option value="manzini">Manzini</md-option>
-          <md-option value="shiselweni">Shiselweni</md-option>
-        </md-select>
-      </md-input-container>
+      
+      <multiselect v-model="searchDefinition" 
+        :options="options" 
+        :multiple="false" 
+        group-values="locations" 
+        group-label="locationType" 
+        placeholder="Type to search a whole bunch of stuff" 
+        track-by="name" 
+        label="name">
+        <span slot="noResult">Oops! No elements found. Consider changing the search query.</span>
+      </multiselect>
 
       <div>
         <md-button class='md-raised md-accent' @click='loadTasks'>Search for clusters</md-button>
@@ -38,15 +25,38 @@
 </template>
 
 <script>
+  import Multiselect from 'vue-multiselect'
   import IrsList from '../list/list.vue'
 
   export default {
     name: 'IrsTasks',
-    components: {'irs-list': IrsList},
+    components: {
+      'irs-list': IrsList,
+      'multiselect': Multiselect
+    },
     data() {
       return {
+        searchDefinition: [],
         spatialScale: '',
         region: '',
+        options: [
+          { 
+            locationType: 'Region',
+            locations: [
+              {name: 'Lubombo'}, {name: 'Shiselweni'}
+            ]
+          },{ 
+            locationType: 'Inkhundla',
+            locations: [
+              {name: 'Good one'}, {name: 'Other one'}
+            ]
+          },{ 
+            locationType: 'Locality',
+            locations: [
+              {name: 'Some Bend'}, {name: 'Small Bend'}
+            ]
+          }
+        ]
       }
     },
     computed: {
