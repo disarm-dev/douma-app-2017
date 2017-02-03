4cabbad21f9ca20cd7da2ab991445f5b51c64d14
Jonathan Smith <jonathan@peoplesized.com>
2017-02-03 13:04:54 +0200

    fixed broken link

--- src/apps/irs_progress/cluster/map.vue
+++ src/apps/irs_progress/cluster/map.vue
@@ -5,8 +5,8 @@
     <h2>Map individual cluster</h2>
     <p>Cluster id: {{$route.params.cluster_id}}</p>
 
-    <md-button @click="$router.push({name: 'irs_progress:cluster:view'})">View</md-button>
-    <md-button @click="$router.push({name: 'irs_progress:cluster:map'})">Map</md-button>
+    <md-button @click="$router.push({name: 'irs_progress:cluster:view', params: {cluster_id: $route.params.cluster_id}})">View</md-button>
+    <md-button @click="$router.push({name: 'irs_progress:cluster:map', params: {cluster_id: $route.params.cluster_id}})">Map</md-button>
   </div>
 </template>
 
