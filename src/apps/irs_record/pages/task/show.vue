f94629a5a21e109f5434641b25bf934576511cef
Jonathan Smith <jonathan@peoplesized.com>
2017-02-10 13:15:20 +0200

    task show

--- src/apps/irs_record/pages/task/show.vue
+++ src/apps/irs_record/pages/task/show.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
     <h1>TaskShow</h1>
-    <p>Cluster id: {{$route.params.cluster_id}}, Task id: {{ $route.params.task_id}}</p>
 
-    <div>
-      {{task}}
-    </div>
+    <md-list class="md-double-line">
+      <template v-for='key in Object.keys(task)'>
+        <md-list-item>
+          <div class="md-list-text-container">
+            <span>{{task[key]}}</span>
+            <span>{{key}}</span>
+          </div>
+        </md-list-item>
+      </template>
+    </md-list>
 
   </div>
 </template>
