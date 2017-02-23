8a2472f4f0b18dc3c4c4c369e6bf8680300ac3e1
Jonathan Smith <jonathan@peoplesized.com>
2017-02-23 13:09:30 +0200

    replace with a user action in the future.

--- src/apps/irs_plan/pages/review/view.vue
+++ src/apps/irs_plan/pages/review/view.vue
@@ -10,9 +10,7 @@
     name: 'ReviewView',
     mounted() {
       if(this.$store.state.irs_plan.clusters.length === 0) {
-        this.$store.dispatch("irs_plan:get_clusters").then(res => {
-          if(res.length === 0) this.$router.push({name:'irs_plan:operational_unit'})
-        })
+        this.$store.dispatch("irs_plan:get_clusters")
       }
     }
   }
