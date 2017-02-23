7b77e4dd762d984e97dc8f100d9440160ad92582
Jonathan Smith <jonathan@peoplesized.com>
2017-02-23 20:45:05 +0200

    route fixes

--- src/apps/irs_plan/routes.js
+++ src/apps/irs_plan/routes.js
@@ -38,18 +38,18 @@ export default [
         ]
       },{
         path: '/irs_plan/create',
-        name: 'irs_plan:create'
+        name: 'irs_plan:create',
         component: CreateView,
         meta: {},
         children: [
           {
             path: 'select_ous',
-            name: 'irs_plan:create:select_ous'
+            name: 'irs_plan:create:select_ous',
             component: SelectOUs,
             meta: {}
           },{
             path: 'preview',
-            name: 'irs_plan:create:preview'
+            name: 'irs_plan:create:preview',
             component: Preview,
             meta: {}
           }
