fb0bc39cab0b86ff345a5f897e79b62aa3da8f40
Jonathan Smith <jonathan@peoplesized.com>
2017-02-28 05:45:52 +0200

    remove unused multiselect

--- src/apps/irs_record/pages/clusters_search/view.vue
+++ src/apps/irs_record/pages/clusters_search/view.vue
@@ -5,44 +5,20 @@
     <md-button @click.native="toggle_view">{{toggle_to_view}}</md-button>
     <md-progress :md-indeterminate='$store.state.irs_record.sync_in_progress'></md-progress>
 
-
     <router-view></router-view>
 
   </div>
-
 </template>
 
 
 <script>
-  import Multiselect from 'vue-multiselect'
 
   export default {
     name: 'ClustersSearchView',
-    components: {
-      'multiselect': Multiselect
-    },
     data() {
       return {
         search_definition: [],
         clusters_to_open: [],
-        search_results: [],
-        search_options: [
-          { 
-            location_type_label: 'Region',
-            locations: [
-              {name: 'Lubombo', location_type: 'region'}, 
-              {name: 'Shiselweni', location_type: 'region'},
-              {name: 'Manzini', location_type: 'region'},
-              {name: 'Hhohho', location_type: 'region'}
-            ]
-          },{ 
-            location_type_label: 'Locality',
-            locations: [
-              {name: 'Locality1', location_type: 'locality'},
-              {name: 'Locality2', location_type: 'locality'}
-            ]
-          }
-        ]
       }
     },
     mounted() {
@@ -74,12 +50,6 @@
       },
       toggle_view() {
         this.$router.push({name: `irs_record:clusters_search:${this.toggle_to_view}`})
-      },
-      on_input(search_query, id) {
-        if (this.search_definition.length) {
-          this.search_results = []
-        }
-
       }
     }
   }
