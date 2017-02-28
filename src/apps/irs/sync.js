0825c55e96de2d318e29b591f78bd3c2df8fe629
Jonathan Smith <jonathan@peoplesized.com>
2017-02-28 08:44:17 +0200

    stub of shared 'get_cluster' sync logic for all IRS applets

---
+++ src/apps/irs/sync.js
@@ -0,0 +1,19 @@
+// Sync logic shared across IRS applets
+
+class IRSSync {
+  constructor(demo_instance_id){
+    this.demo_instance_id = demo_instance_id
+  }
+
+  get_clusters() {
+    // Try to read from LocalDB
+    // If Clusters, then return and trigger a background refresh
+    // If no Clusters, then trigger a background/foreground refresh
+    // When refresh complete, notify user and ask if they want to update
+  }
+
+  _notify_user() {
+    
+  }
+
+}
