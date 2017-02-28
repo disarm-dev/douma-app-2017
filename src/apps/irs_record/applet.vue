24e1e972ed0876a9c1ea8cfc153678f3fb1a61d5
Jonathan Smith <jonathan@peoplesized.com>
2017-02-28 14:12:55 +0200

    irs_record starting to use irs_tasker clusters store...

--- src/apps/irs_record/applet.vue
+++ src/apps/irs_record/applet.vue
@@ -6,7 +6,7 @@
   export default {
     name: 'IrsRecordApplet',
     mounted() {
-      this.$store.dispatch('irs_record:configure_sync', this.$store.state.meta.demo_instance_id)
+      this.$store.dispatch("irs_tasker:get_clusters", {demo_instance_id: this.$store.state.meta.demo_instance_id})
     }
   }
 </script>
