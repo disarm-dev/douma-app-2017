c3833e8652e810b85c5615f4649df9b2a823fe53
Jonathan Smith <jonathan@peoplesized.com>
2017-03-07 15:29:17 +0200

    more mark-mode-buttons. before they are removed...

--- src/apps/irs_plan/pages/create/mark_mode_buttons.vue
+++ src/apps/irs_plan/pages/create/mark_mode_buttons.vue
@@ -1,20 +1,38 @@
 <template>
   <div>
-    <md-button @click.native='set_mark_mode("add")' :disabled='is_disabled("add")'>mark to add</md-button>
-    <md-button @click.native='set_mark_mode("remove")' :disabled='is_disabled("remove")'>mark to remove</md-button>
+    <md-button 
+      @click.native='set_mark_mode("add")' 
+      :disabled='is_disabled("add")'
+      :class="{'md-primary': is_active('add')}">
+      mark to add
+    </md-button>
+    <md-button 
+      @click.native='set_mark_mode("remove")' 
+      :disabled='is_disabled("remove")'
+      :class="{'md-primary': is_active('remove')}">
+      mark to remove
+    </md-button>
   </div>
 </template>
 
 <script>
   export default {
     named: 'MarkModeButtons',
+    computed: {
+      current_mode() {
+        return this.$store.state.irs_plan.mark_mode
+      }
+    },
     methods: {
+      is_active(mark_mode) {
+        return this.current_mode === mark_mode
+      },
       is_disabled(mark_mode) {
-        if (this.$store.state.irs_plan.mark_mode && this.$store.state.irs_plan.mark_mode !== mark_mode) return true
+        if (this.current_mode && this.current_mode !== mark_mode) return true
         return false
       },
       set_mark_mode(mark_mode) {
-        if (this.$store.state.irs_plan.mark_mode === mark_mode) mark_mode = null
+        if (this.current_mode === mark_mode) mark_mode = null
         this.$store.commit('irs_plan:set_mark_mode', mark_mode)
       }
     }
