002a01ded146dd5ff217b575fa354cb33db7a679
Nicolai Davies <nicolaidavies.nd@gmail.com>
2017-03-14 10:42:06 +0200

    Add legend to map

--- src/apps/climate/pages/map.vue
+++ src/apps/climate/pages/map.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="map"></div>
+  <div id="weather-map"></div>
 </template>
 
 <script>
@@ -16,6 +16,7 @@
     data () {
       return {
         map: null,
+        legend: null,
         tile_layer: null,
         opacity_slider: null
       }
@@ -28,7 +29,7 @@
     },
     methods: {
       create_map() {
-        this.map = Leaflet.map('map', {
+        this.map = Leaflet.map('weather-map', {
           center: {lat: -18.656654486540006, lng: 29.575195312500004},
           zoom: 6
         });
@@ -39,7 +40,6 @@
 
         // this.opacity_slider = new L.Control.opacitySlider()
         // this.map.addControl(opacity_slider)
-
       },
       change_tile_layer() {
         if (!this.layer || !this.date) return
@@ -51,16 +51,104 @@
 
         this.tile_layer = L.tileLayer(this.tile_url, {tms: true})//, opacity: 0.6})
         this.tile_layer.addTo(this.map)
+
+        this.update_legend()
         
         // this.opacity_slider.setOpacityLayer(this.tile_layer)
+      },
+      update_legend() {
+        
+        fetch('http://130.211.51.103:3000/processor/palettes/' + this.layer.slug + '_palette')
+          .then(res => res.text())
+          .then(res => {
+            let labels = []
+            let colors = []
+            let title = null
+
+            res.split('\n').map((line) => {
+              let parts = line.split(',')
+
+              if (parts[0]) {
+                labels.push(parts[0])
+                colors.push(`rgb(${parts[1]}, ${parts[2]}, ${parts[3]})`)
+              }
+            })
+
+            labels = labels.map((label) => {
+              switch (this.layer.slug) {
+                case 'LST':
+                  title = '°C'
+                  label = Math.round((0.02 * label) - 273.15)
+                  break;
+                case 'PRECIP':
+                  title = 'Max daily mm'
+                  label = Math.round(label / 5)
+                  break;
+                case 'EVI':
+                  title = 'Max daily mm'
+                  let customRound = function(number, precision) {
+                      var factor = Math.pow(10, precision);
+                      var tempNumber = number * factor;
+                      var roundedTempNumber = Math.round(tempNumber);
+                      return roundedTempNumber / factor;
+                  }
+                  label = customRound(label, 3)
+                  break;
+              }
+              return label
+            })
+
+            if (this.legend) {
+              this.map.removeControl(this.legend)
+              this.legend = null
+            }
+            
+            this.legend = Leaflet.control({position: 'bottomright'});
+
+            this.legend.onAdd = function (map) {
+
+                var div = Leaflet.DomUtil.create('div', 'info legend')
+
+                if (title) {
+                  div.innerHTML += `<p><b>${title}</b></p>`
+                }
+
+                for (var i = 0; i < labels.length; i++) {
+
+                    div.innerHTML += 
+                        '<i style="background:' + colors[i] + '"></i> ' +
+                        labels[i] + (labels[i + 1] !== undefined ? '&ndash;' + labels[i + 1] + '<br>' : '+');
+                }
+
+                div.style = "background: white; border-radius: 5px; padding: 10px;"
+                return div;
+            };
+
+            this.legend.addTo(this.map);
+          })
+
+
       }
     }
   }
 </script>
 
-<style scoped>
-  #map {
+<style>
+  #weather-map {
     min-height: calc(100vh - 270px);
     z-index: 0;
+  }
+
+  .legend {
+    line-height: 18px;
+    color: #555;
+  }
+
+  .legend i {
+      width: 18px;
+      height: 18px;
+      float: left;
+      margin-right: 8px;
+      opacity: 0.7;
   }
 </style>
