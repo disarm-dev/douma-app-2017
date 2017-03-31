51d6a7b59c04add810f3a26a0ffd41db3c83979c
Jonathan Smith <jonathan@peoplesized.com>
2017-03-31 16:33:26 +0200

    at least this view is a bit simpler

--- src/apps/irs_plan/pages/create/view.vue
+++ src/apps/irs_plan/pages/create/view.vue
@@ -6,18 +6,8 @@
         <p>There are SOME clusters in the selected localities. There are A FEW structures.</p>
       </div>
 
-      <!-- <md-button-toggle md-single>
-        <md-button v-for='action in actions' @click.native='set_action(action)' :ref='action.command'>
-          {{action.title}}
-        </md-button>
-      </md-button-toggle> -->
-
       <div class="controls">
         <formal-bulk></formal-bulk>
-        <!-- DYNAMIC COMPONENT -->
-        <!-- <keep-alive>
-          <component :is='selected_component'></component>
-        </keep-alive> -->
       </div>
     </div>
 
@@ -27,28 +17,11 @@
 <script>
   import FormalBulk from './formal_bulk.vue'
   import Draw from './draw.vue'
-  import Result from './result.vue'
   import {mapState} from 'vuex'
 
   export default {
     name: 'AreasView',
-    components: {FormalBulk, Draw, Result},
-    props: [],
-    mounted() {
-      // this.$nextTick(() => { // Hack for vue-material tabs
-      //   this.$refs['FormalBulk'][0].$el.click()
-      // })
-    },
-    data() {
-      return {
-        // Actions
-        actions: [
-          { title: 'Multiple', command: 'FormalBulk' },
-          // { title: 'hand-draw', command: 'Draw' },
-          { title: 'Result', command: 'Result' }
-        ],
-      }
-    },
+    components: {FormalBulk, Draw},
     computed: {
       ...mapState({
         selected_component: state => state.irs_plan.selected_component
@@ -65,4 +38,7 @@
 <style>
   .container { margin: 10px; }
   .controls { background: #eaeaea; padding: 5px;}
+    #map {
+    min-height: 80vh;
+  }
 </style>
