e53adf4783d6f5c30d76d44ab4a9a71cea905c55
Jonathan Smith <jonathan@peoplesized.com>
2017-04-05 07:57:06 +0200

    renamed variable to include _ids

--- src/apps/irs_plan/store.js
+++ src/apps/irs_plan/store.js
@@ -11,8 +11,8 @@ export default {
     // Data - 'areas' are formal areas, like 'localities'
     risk_slider_value: null,
     formal_areas: [],
-    areas_included_by_click: [], // TODO: @refac Rename to '_ids'
-    areas_excluded_by_click: [], // TODO: @refac Rename to '_ids'
+    area_ids_included_by_click: [],
+    area_ids_excluded_by_click: [],
 
     // Calculations
     selected_cluster_stats: {
@@ -34,10 +34,10 @@ export default {
       const bulk_selected_ids = getters['irs_plan:bulk_selected_ids']
 
       // add included by click
-      let result = bulk_selected_ids.concat(state.areas_included_by_click)
+      let result = bulk_selected_ids.concat(state.area_ids_included_by_click)
 
       // remove excluded by click
-      state.areas_excluded_by_click.forEach(area_id => {
+      state.area_ids_excluded_by_click.forEach(area_id => {
         const index = result.findIndex(i => i === area_id)
         if (index !== -1) {
           result.splice(index, 1)
@@ -62,18 +62,18 @@ export default {
       state.all_clusters = []
     },
     'irs_plan:add_included': (state, area_id) => {
-      state.areas_included_by_click.push(area_id)
+      state.area_ids_included_by_click.push(area_id)
     },
     'irs_plan:remove_included': (state, area_id) => {
-      let index = state.areas_included_by_click.findIndex(i => i === area_id)  
-      state.areas_included_by_click.splice(index, 1)
+      let index = state.area_ids_included_by_click.findIndex(i => i === area_id)  
+      state.area_ids_included_by_click.splice(index, 1)
     },
     'irs_plan:add_excluded': (state, area_id) => {
-      state.areas_excluded_by_click.push(area_id)
+      state.area_ids_excluded_by_click.push(area_id)
     },
     'irs_plan:remove_excluded': (state, area_id) => {
-      let index = state.areas_excluded_by_click.findIndex(i => i === area_id)  
-      state.areas_excluded_by_click.splice(index, 1)
+      let index = state.area_ids_excluded_by_click.findIndex(i => i === area_id)  
+      state.area_ids_excluded_by_click.splice(index, 1)
     },
     'irs_plan:set_selected_cluster_stats': (state, stats) => {
       state.selected_cluster_stats = Object.assign({}, state.selected_cluster_stats, stats)
@@ -81,9 +81,9 @@ export default {
   },
   actions: {
     'irs_plan:area_click': (context, area_id) => {
-      if (context.state.areas_included_by_click.includes(area_id)) {
+      if (context.state.area_ids_included_by_click.includes(area_id)) {
         context.commit('irs_plan:remove_included', area_id)
-      } else if (context.state.areas_excluded_by_click.includes(area_id)) {
+      } else if (context.state.area_ids_excluded_by_click.includes(area_id)) {
         context.commit('irs_plan:remove_excluded', area_id)
       } else if (context.getters['irs_plan:bulk_selected_ids'].includes(area_id)){
         context.commit('irs_plan:add_excluded', area_id)
