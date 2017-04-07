c33d450be3ff5aaa8ff32e4717c4f8f416c98f46
Nicolai Davies <nicolaidavies.nd@gmail.com>
2017-04-07 11:22:42 +0200

    Working towards suggesting foci

--- src/apps/new_foci/store.js
+++ src/apps/new_foci/store.js
@@ -5,11 +5,20 @@ export default {
     focis: focis
   },
   mutations: {
-    'foci:update_cluster': (state, foci) => {
+    'foci:update_foci': (state, foci) => {
       let index = state.focis.findIndex(f => f.properties._id === foci.properties._id)
       state.focis.splice(index, 1, foci)
     }
   },
   actions: {
+    'foci:accept_suggestion': (context, foci) => {
+      foci.geometry = foci.properties.suggested.geometry
+      delete foci.properties.suggested
+      context.commit('foci:update_cluster', foci)
+    },
+    'foci:disard_suggestion': (context, foci) => {
+      delete foci.properties.suggested
+      context.commit('foci:update_cluster', foci)
+    }
   }
 }
