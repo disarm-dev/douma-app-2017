c16d6555af5a236322bc610710cfd5136326a00a
Nicolai Davies <nicolaidavies.nd@gmail.com>
2017-04-11 10:39:39 +0200

    Cache local areas and clusters

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -3,5 +3,15 @@ module.exports = {
   root: 'dist',
   maximumFileSizeToCacheInBytes: 14194304,
   navigateFallback: 'index.html',
-  importScripts: ['offline-analytics.js']
+  importScripts: ['offline-analytics.js'],
+  runtimeCaching: [
+    {
+      urlPattern: /https:\/\/douma-api\.herokuapp.com\/v2\/local_areas\/*/,
+      handler: 'cacheFirst'
+    },
+    {
+      urlPattern: /https:\/\/douma-api\.herokuapp.com\/v2\/clusters\/all\/*/,
+      handler: 'cacheFirst'
+    }
+  ]
 }
