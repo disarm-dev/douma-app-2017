3bd70935e734b67fcb78f88e4e6bc7aef0e182f6
Jonathan Smith <jonathan@peoplesized.com>
2017-04-24 14:51:56 +0200

    doc

--- src/lib/cluster_results.js
+++ src/lib/cluster_results.js
@@ -1,9 +1,13 @@
-
-const cluster_results = (all_clusters, all_selected_area_ids) => {
+/**
+ * Find clusters within the selected areas of interest
+ * @param all_clusters Array of all the clusters
+ * @param all_selected_area_ids Array of area_ids for selected areas of interest
+ * @return Array Clusters
+ */
+function cluster_results(all_clusters, all_selected_area_ids) {
   return all_clusters.filter(cluster => {
-    return  all_selected_area_ids.includes(cluster.properties.area_id)
+    return all_selected_area_ids.includes(cluster.properties.area_id)
   })
 }
-
 
 export default cluster_results
