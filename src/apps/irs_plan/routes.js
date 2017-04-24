d1d10ef60eedb3163252ba05a3b566e4c5b2c45c
Jonathan Smith <jonathan@peoplesized.com>
2017-04-24 15:07:05 +0200

    removed matching routes from removed views

--- src/apps/irs_plan/routes.js
+++ src/apps/irs_plan/routes.js
@@ -1,11 +1,6 @@
 import IrsPlanApplet from './applet.vue'
 
 import CreateView from './pages/create/view.vue'
-// import Preview from './pages/create/preview.vue'
-
-import ReviewView from './pages/review/view.vue'
-import ReviewMap from './pages/review/map.vue'
-import ReviewList from './pages/review/list.vue'
 
 import store from '../../store'
 
@@ -22,25 +17,6 @@ export default [
         name: 'irs_plan:create',
         component: CreateView,
         meta: {title: 'Create'},
-      },{
-        path: 'review',
-        name: 'irs_plan:review',
-        redirect: '/irs_plan/review/map',
-        component: ReviewView,
-        meta: {},
-        children: [
-          {
-            path: 'map',
-            name: 'irs_plan:review:map',
-            component: ReviewMap,
-            meta: {title: 'IRS - Review clusters', type: 'map'}
-          },{
-            path: 'list',
-            name: 'irs_plan:review:list',
-            component: ReviewList,
-            meta: {type: 'list'}
-          }
-        ]
       }
     ]
   }
