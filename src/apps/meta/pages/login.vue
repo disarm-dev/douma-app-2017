8587f0ccede9817db9e29db06591f1caa325b334
Jonathan Smith <jonathan@peoplesized.com>
2017-05-09 15:40:26 +0200

    basic login form

--- src/apps/meta/pages/login.vue
+++ src/apps/meta/pages/login.vue
@@ -1,18 +1,104 @@
 <template>
+  <div class='login'>
+
+    <md-card>
+      <md-card-content>
+        <form novalidate @submit.stop.prevent="login">
+          <div>
+            <md-icon class="login-icon">person</md-icon>
+          </div>
+          <p class="md-body-1 login-text">Welcome to the DiSARM {{country}}</p>
+
+          <md-input-container>
+            <label>Username</label>
+            <md-input v-model="username" type="username"></md-input>
+          </md-input-container>
+
+          <md-input-container>
+            <label>Password</label>
+            <md-input v-model="password" type="password"></md-input>
+          </md-input-container>
+
+          <md-button class="md-accent" :disabled='disabled' @click.native="login()">Login</md-button>
+
+          
+        </form>
+     </md-card-content>
+    </md-card>
+
+  </div>
 </template>
 
 <script>
-export default {
+  import {generate_demo_instance_id} from '../../../lib/demo_instance_id'
 
-  name: 'login',
+  export default {
+    data() {
+      return {
+        msg: 'Please login below',
+        disabled: false,
+        username: '',
+        password: '',
+      }
+    },
+    computed: {
+      country() {
+        return this.$store.state.instance_config.name
+      }
+    },
+    methods: {
+      login() {
+        this.disabled = true
+        
+        this.$store.dispatch('meta:login', user).then(() => {
+          this.disabled = false
+          this.continue()
+        })
+        .catch(e => {
+          this.disabled = false
+          console.log(e)
+        })
 
-  data () {
-    return {
-
-    };
+      },
+      continue() {
+        if (this.$store.state.meta.previousRoute) {
+          let {name} = this.$store.state.meta.previousRoute
+          this.$router.push({name})  
+        } else {
+          this.$router.push('/')  
+        }
+      }
+    }
   }
-};
 </script>
 
-<style lang="css" scoped>
+<style>
+  .login {
+    max-width: 500px;
+    margin: 1em auto;
+    padding: 0 0.5em;
+  }
+
+  .login-icon {
+    margin: 0 auto;
+    display: block;
+    height: 50px;
+    width: 50px;
+    font-size: 50px;
+  }
+
+  .login-text {
+    padding-top: 0.5em;
+    text-align: center;
+  }
+
+  .login-button {
+    margin: 0 auto;
+    display: block;
+  }
+
+  .text-center {
+    text-align: center;
+  }
+
 </style>
