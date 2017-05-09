fe3288af898b1aeae5009a44678e4f37ca685755
Jonathan Smith <jonathan@peoplesized.com>
2017-05-09 15:26:53 +0200

    login and home page stubs

--- src/apps/meta/routes.js
+++ src/apps/meta/routes.js
@@ -1,8 +1,26 @@
-import Home from './applet.vue'
+import applet from './applet.vue'
+import home from './pages/home.vue'
+import login from './pages/login.vue'
 
 export default [
   {
     path: '/meta',
-    component: Home,
+    component: applet,
+    redirect: '/meta/home',
+    name: 'meta',
+    meta: {title: 'User', icon: 'person'},
+    children: [
+      {
+        path: 'home',
+        name: 'meta:home',
+        component: home,
+        meta: {title: 'Home'}
+      },
+      {
+        path: 'login',
+        name: 'meta:login',
+        component: login,
+      }
+    ]
   }
 ]
