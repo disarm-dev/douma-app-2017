20e84cfaa2651ba0e87609bd38c07426a34ab603
Jonathan Smith <jonathan@peoplesized.com>
2017-05-09 09:13:39 +0200

    add `launch` function to index.js

--- src/index.js
+++ src/index.js
@@ -21,23 +21,44 @@ Raven
   .install()
 Raven.setExtraContext({DOUMA_version: COMMIT_HASH})
 
-// Create a bunch of themes matching the routes
-configureThemes()
 
-Vue.use(ClientTable, {}, false);
+const launch = (instance_config) => {
+  // Create a bunch of themes matching the routes
+  configureThemes()
 
-// Make DOUMA App
-const router = configureRouter()
+  Vue.use(ClientTable, {}, false);
 
-const douma_app = new Vue({
-  el: '#douma',
-  router, 
-  store,
-  render: createElement => createElement(DoumaComponent),
+  // Make DOUMA App
+  const router = configureRouter()
+
+  const douma_app = new Vue({
+    el: '#douma',
+    router, 
+    store,
+    render: createElement => createElement(DoumaComponent),
+  })
+
+  douma_app.$store.state.instance_config = instance_config
+
+  // ServiceWorker
+  configureServiceWorker(douma_app)
+
+  // Keep track of what version we're working on
+  console.info('DOUMA version: ' + COMMIT_HASH)
+}
+
+const subdomain = document.domain.split('.')[0]
+console.log('subdomain', subdomain)
+
+fetch(`/config/${subdomain}.json`).then(res => {
+  if (res.status === 404) {
+    throw new Error(`Cannot find configuration file for ${subdomain}`)
+  }
+  res.json() 
+}).then(json => {
+  launch(json)
 })
 
-// ServiceWorker
-configureServiceWorker(douma_app)
 
-// Keep track of what version we're working on
-console.info('DOUMA version: ' + COMMIT_HASH)
+
+
