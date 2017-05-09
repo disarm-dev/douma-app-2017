b5b1bb5a36e37897b9c6c84cbd6fe1287a517cce
Jonathan Smith <jonathan@peoplesized.com>
2017-05-09 12:37:07 +0200

    simpler test. still doesn't run

--- test/unit/specs/Hello.spec.js
+++ test/unit/specs/Hello.spec.js
@@ -1,11 +1,18 @@
-import Vue from 'vue'
-import Hello from '@/components/Hello'
+// import Vue from 'vue'
+// import Hello from '@/apps/meta/pages/home.vue'
 
-describe('Hello.vue', () => {
-  it('should render correct contents', () => {
-    const Constructor = Vue.extend(Hello)
-    const vm = new Constructor().$mount()
-    expect(vm.$el.querySelector('.hello h1').textContent)
-      .to.equal('Welcome to Your Vue.js App')
+// describe('home.vue', () => {
+//   it('should render correct contents', () => {
+//     const Constructor = Vue.extend(Hello)
+//     const vm = new Constructor().$mount()
+//     expect(vm.$el.querySelector('.hello h1').textContent)
+//       .to.equal('Welcome to Your Vue.js App')
+//   })
+// })
+
+
+describe('not a real test', () => {
+  it('it be true', () => {
+    expect(1).to.equal(1)
   })
 })
