18b41d7e0e0a9b3734e26008497a3cc341c14adc
Nicolai Davies <nicolaidavies.nd@gmail.com>
2017-05-10 08:36:25 +0200

    Service worker back again

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -8,6 +8,7 @@ var CopyWebpackPlugin = require('copy-webpack-plugin')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
 var OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
+var SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin')
 
 var env = process.env.NODE_ENV === 'testing'
   ? require('../config/test.env')
@@ -95,8 +96,10 @@ var webpackConfig = merge(baseWebpackConfig, {
         ignore: ['.*']
       },
       { from: './src/offline-analytics.js' },
+      { from: './src/manifest.json' },
       { from: './node_modules/sw-offline-google-analytics/build/offline-google-analytics-import.min.js' },
-    ])
+    ]),
+    new SWPrecacheWebpackPlugin(require('../sw-precache-config.js'))
   ]
 })
 
