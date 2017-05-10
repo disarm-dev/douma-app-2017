a76cd8951d0386c347aa55b6b9071a4636d1cf3f
Jonathan Smith <jonathan@peoplesized.com>
2017-05-10 10:36:07 +0200

    getting location integrated

--- src/apps/irs_record_point/pages/record.vue
+++ src/apps/irs_record_point/pages/record.vue
@@ -2,27 +2,35 @@
   <div>
     <h1>RECORD {{country}}</h1>
     <h3>Type: {{type}}</h3>
+    <location_record v-on:change='update_location'></location_record>
   </div>
 </template>
 
 <script>
-export default {
+  import location_record from '@/components/location.vue'
 
-  name: 'home',
+  export default {
 
-  data () {
-    return {
-    }
-  },
-  computed: {
-    country() {
-      return this.$store.state.instance_config.name
+    name: 'home',
+    components: {location_record},
+    data () {
+      return {
+      }
     },
-    type() {
-      return this.$store.state.instance_config.applets.irs_record_point.location_type
+    computed: {
+      country() {
+        return this.$store.state.instance_config.name
+      },
+      type() {
+        return this.$store.state.instance_config.applets.irs_record_point.location_type
+      }
+    },
+    methods: {
+      update_location(location) {
+        console.log(location)
+      }
     }
   }
-};
 </script>
 
 <style lang="css" scoped>
