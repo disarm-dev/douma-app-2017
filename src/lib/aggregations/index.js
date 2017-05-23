53eb3b9421ab3e1d3f49d02696aa38062ac76aca
Jonathan Smith <jonathan@peoplesized.com>
2017-05-23 11:16:04 +0200

    all the other aggregations. empty

---
+++ src/lib/aggregations/index.js
@@ -0,0 +1,6 @@
+import bwa from './bwa.aggregations.js'
+import nam from './nam.aggregations.js'
+import swz from './swz.aggregations.js'
+import zwe from './zwe.aggregations.js'
+
+export default {bwa, nam, swz, zwe}
