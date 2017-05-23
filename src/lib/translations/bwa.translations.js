bc285d9e43d040c3bd28f34d5b34becdd7c21cb2
Nicolai Davies <nicolaidavies.nd@gmail.com>
2017-05-23 11:30:29 +0200

    Add Botswana regions to IRS Plan

--- src/lib/translations/bwa.translations.js
+++ src/lib/translations/bwa.translations.js
@@ -1,6 +1,24 @@
 import Base from './base.translations.js'
 
 export default class extends Base {
+  
+  operational_units(feature_collection) {
+
+    feature_collection.features = feature_collection.features.map(ou => {
+      return {
+        type: ou.type,
+        geometry: ou.geometry,
+        properties: {
+          name: `${ou.properties.Name}. ${ou.properties.VILLAGE}`,
+          id: ou.properties.OBJECTID_1,
+        }
+      }
+    })
+
+    return feature_collection
+    
+  }
+
   sprayed_count() {
     return this.responses.reduce((acc, response, index) => {
       // debugger
