c914e31b21f286121d794d519a9107a15c751186
Jonathan Smith <jonathan@peoplesized.com>
2017-05-26 10:47:48 +0200

    don't clear localstorage on logout

--- src/apps/meta/pages/logout.vue
+++ src/apps/meta/pages/logout.vue
@@ -9,9 +9,7 @@
   export default {
     mounted() {
       this.$store.dispatch('meta/logout').then(() => {
-        // TODO @feature Use Vuex to clear out localStorage instead of forcing it like this
-        localStorage.clear()
-        this.$router.push('/meta/login')
+        this.$router.push('/')
       })
     }
   }
