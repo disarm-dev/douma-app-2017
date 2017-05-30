ed8ef08c3d954157cdaa1be3b1534d99e5c925cd
Jonathan Smith <jonathan@peoplesized.com>
2017-05-30 09:36:58 +0200

    this is a warning

--- src/lib/validations/nam.validations.js
+++ src/lib/validations/nam.validations.js
@@ -1,20 +1,20 @@
 export default [
-  { 
+  {
     name: 'total_sprayed',
     fn: (f) => {
       if (f.sprayable_unsprayed === 'yes') return true
-      
+
       return f.numbersprayed_ddt + f.numbersprayed_delta == f.number_sprayable
     },
     message: 'Total sprayed does not equal `ddt`+ `deltamethrin`.',
-    type: "error",
+    type: "warning",
     relevant_questions: ["sprayable_unsprayed", "numbersprayed_ddt", "numbersprayed_delta", "number_sprayable"]
   },
   {
     name: 'sprayable_unsprayed',
     fn: (f) => {
       if (f.sprayable_unsprayed === 'yes') return true
-      
+
       return f.numbersprayed_ddt + f.numbersprayed_delta + f.number_unsprayed == f.number_sprayable
     },
     message: "Number of unsprayed structures doesn't add up",
