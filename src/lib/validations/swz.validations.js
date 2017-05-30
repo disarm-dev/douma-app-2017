de816d21a48bc8eac0abd4b7071dc55ff0caa080
Nicolai Davies <nicolaidavies.nd@gmail.com>
2017-05-30 17:11:03 +0200

    Add SWZ Validations

--- src/lib/validations/swz.validations.js
+++ src/lib/validations/swz.validations.js
@@ -1,13 +1,32 @@
 export default [
   {
     name: 'total_sprayed',
-    fn: (form_data) => {
-      return f.number_of_structures_sprayed + f.number_of_structures_unsprayed == f.number_structures_total
+    fn: (f) => {
+      return f.number_of_structures_sprayed <= f.number_structures_total
+    },
+    message: 'Number of sprayed structures is larger than total structures.',
+    relevant_questions: ['number_of_structures_sprayed', 'number_structures_total'],
+    type: "warning"
+  },
+  {
+    name: 'total_sprayed',
+    fn: (f) => {
+      // fix this one
+      return f.number_of_structures_sprayed <= f.number_structures_total
     },
     message: 'Total structures does not equal structures sprayed and unsprayed.',
-    stopping_power: "soft",
-    input_questions: [],
-    output_question: ''
+    relevant_questions: ['number_of_structures_sprayed', 'number_structures_total'],
+    type: "warning"
+  },
+  {
+    name: 'total_sprayed',
+    fn: (f) => {
+      if (f.number_of_structures_unsprayed === 'no') {
+        return  f.number_structures_total === f.number_of_structures_sprayed
+      }
+    },
+    message: 'Total structures does not equal structures sprayed and unsprayed.',
+    relevant_questions: ['number_of_structures_sprayed', 'number_structures_total'],
+    type: "warning"
   }
 ]
-
