7e9552ca4f1c3658beb276adfe0914a023a9bd2b
Nicolai Davies <nicolaidavies.nd@gmail.com>
2017-05-31 11:53:50 +0200

    Changes

--- src/apps/irs_plan/pages/view.vue
+++ src/apps/irs_plan/pages/view.vue
@@ -1,6 +1,13 @@
 <template>
   <div class='container'>
     <h1>IRS Plan</h1>
+    
+    <md-input-container>
+      <label for="hierarchy">Select Spatial Level</label>
+      <md-select name="hierarchy" id="hierarchy" v-model="hierarchy_name">
+        <md-option v-for="{name} in hierarchies" :value="name" :key="name">{{name}}</md-option>
+      </md-select>
+    </md-input-container>
 
     <div id="map"></div>
 
@@ -21,11 +28,13 @@
 </template>
 
 <script>
-  import Leaflet from 'leaflet'
+  import mapboxgl from 'mapbox-gl'
   import Translations from '@/lib/translations'
   import download from 'downloadjs'
   import json2csv from 'json2csv'
   import moment from 'moment'
+
+  mapboxgl.accessToken = 'pk.eyJ1Ijoibmljb2xhaWRhdmllcyIsImEiOiJjaXlhNWw1NnkwMDJoMndwMXlsaGo5NGJoIn0.T1wTBzV42MZ1O-2dy8SpOw';
 
   export default {
     name: 'IRSPlan',
@@ -33,14 +42,26 @@
       return {
         _map: null,
         _regions_layer: null,
-        regions: []
+        regions: [],
+        hierarchy_name: '',
+        entities: []
       }
     },
     mounted() {
       this.create_map()
-      this.get_local_areas()
     },
     computed: {
+      hierarchies() {
+        return this.$store.state.instance_config.spatial_hierarchy
+      },
+      hierarchy() {
+        if (this.hierarchy_name) {
+          return this.$store.state.instance_config.spatial_hierarchy.find(({name}) => name == this.hierarchy_name)
+        }
+      },
+      instance_config() {
+        return this.$store.state.instance_config
+      },
       slug() {
         return this.$store.state.instance_config.slug.toLowerCase()
       },
@@ -70,7 +91,54 @@
         return Object.keys(this.tableData[0])
       }
     },
+    watch: {
+      'hierarchy': 'render_hierarchy'
+    },
     methods: {
+      create_map() {
+        this._map = new mapboxgl.Map({
+          container: 'map',
+          style: 'mapbox://styles/mapbox/streets-v9',
+          center: [this.instance_config.map_focus.centre.lng, this.instance_config.map_focus.centre.lat],
+          zoom: this.instance_config.map_focus.zoom
+        });
+      },
+
+      render_hierarchy() {
+        this.hierarchies.forEach(({name}) => {
+          if (this._map.getLayer(name)) {
+            this._map.removeLayer(name)
+            this._map.removeSource(name)
+          }
+        })
+
+        fetch(`/static/spatial_hierarchy/swz.${this.hierarchy_name}.geojson`)
+        .then(res => res.json())
+        .then((geojson) => {
+          this.entities = geojson.features
+          this._map.addLayer({
+            id: this.hierarchy_name,
+            type: 'fill',
+            source: {
+              type: 'geojson',
+              data: geojson
+            },
+            'paint': {
+              'fill-color': '#088',
+              'fill-opacity': 0.8,
+              'fill-outline-color': 'black'
+            }
+          })
+        })
+
+        this._map.on('click', this.hierarchy_name, function (e) {
+          let feature = e.features[0]
+
+          console.log(feature)
+          console.log(this.hierarchy)
+        });
+      },
+
       clear_plan() {
         this.$store.commit('irs_plan/clear_plan')
         this._regions_layer.setStyle(this.style_function)
@@ -87,48 +155,37 @@
             this.add_regions(translated_geojson)
           })
       },
-      create_map() {
-        this._map = Leaflet.map('map', {
-          tms: true,
-          center: this.map_focus.centre,
-          zoom: this.map_focus.zoom
-        });
+      // add_regions(geojson) {
 
-        const url = 'https://api.mapbox.com/styles/v1/onlyjsmith/civ9t5x7e001y2imopb8c7p52/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoib25seWpzbWl0aCIsImEiOiI3R0ZLVGtvIn0.jBTrIysdeJpFhe8s1M_JgA'
+      //   this._regions_layer = L.geoJSON(geojson, {
+      //     pointToLayer: (feature, latlng) => {
+      //       return L.circleMarker(latlng, {});
+      //     },
+      //     style: this.style_function
+      //   })
 
-        Leaflet.tileLayer(url).addTo(this._map);
-      },
-      add_regions(geojson) {
+      //   this._regions_layer.on('click', (object, a) => {
+      //     let id = object.layer.feature.properties.id
 
-        this._regions_layer = L.geoJSON(geojson, {
-          pointToLayer: (feature, latlng) => {
-            return L.circleMarker(latlng, {});
-          },
-          style: this.style_function
-        })
+      //     this.$store.commit('irs_plan/toggle_selected_region', id)
 
-        this._regions_layer.on('click', (object, a) => {
-          let id = object.layer.feature.properties.id
+      //     this._regions_layer.setStyle(this.style_function)
+      //   })
 
-          this.$store.commit('irs_plan/toggle_selected_region', id)
-
-          this._regions_layer.setStyle(this.style_function)
-        })
-
-        this._regions_layer.addTo(this._map)
-      },
-      style_function(feature, layer) {
-        if (this.selected_region_ids.includes(feature.properties.id)) {
-          return {
-            color: 'red',
-            weight: 0.8
-          }
-        }
-        return {
-          color: '#756bb1',
-          weight: 0.8
-        }
-      },
+      //   this._regions_layer.addTo(this._map)
+      // },
+      // style_function(feature, layer) {
+      //   if (this.selected_region_ids.includes(feature.properties.id)) {
+      //     return {
+      //       color: 'red',
+      //       weight: 0.8
+      //     }
+      //   }
+      //   return {
+      //     color: '#756bb1',
+      //     weight: 0.8
+      //   }
+      // },
       download_plan() {
         const data = this.tableData
         const fields = this.tableColumns
