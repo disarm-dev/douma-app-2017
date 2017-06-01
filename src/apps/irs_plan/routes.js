60c30b2c65cf41b320e9a9b814c10378f816ea19
Jonathan Smith <jonathan@peoplesized.com>
2017-06-01 11:53:04 +0200

    fix routes to restore IRS plan to DOUMA.vue^

--- src/apps/irs_plan/routes.js
+++ src/apps/irs_plan/routes.js
@@ -5,13 +5,12 @@ export default [
   {
     path: '/irs/plan',
     component: review,
-    name: 'irs_plan:review',
+    name: 'irs_plan',
     meta: {title: 'IRS Plan', icon: 'assignment_turned_in'},
   },
   {
     path: '/irs/plan/edit',
     component: edit,
-    name: 'irs_plan:edit',
-    meta: {title: 'IRS Plan', icon: 'assignment_turned_in'},
+    name: 'irs_plan:edit'
   }
 ]
