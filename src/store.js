64358ccb0b3993cc34c484d2e5a0f002a8ef908e
Jonathan Smith <jonathan@peoplesized.com>
2017-06-01 11:53:39 +0200

    add global cache, not persisted to vuex/localStorage^

--- src/store.js
+++ src/store.js
@@ -3,11 +3,24 @@ import Vuex from 'vuex'
 import createPersistedState from 'vuex-persistedstate'
 Vue.use(Vuex)
 
+const persisted = createPersistedState({
+  setState: (key, state) => {
+    let copy = Object.assign({}, state)
+
+    // Important
+    copy.cache = {}
+
+    localStorage.setItem(key, JSON.stringify(copy))
+  }
+})
+
 function create_store(instance_stores) {
+
   return new Vuex.Store({
     modules: instance_stores,
-    plugins: [createPersistedState()],
+    plugins: [persisted],
     state: {
+      cache: {},
       snackbar: {},
       loading: false,
       sw_message: {message: 'null', title: 'null'},
@@ -25,6 +38,12 @@ function create_store(instance_stores) {
       },
       'root:set_instance_config': (state, instance_config) => {
         state.instance_config = instance_config
+      },
+      'root:set_cache': (state, {key, value}) => {
+        state.cache[key] = value
+      },
+      'root:clear_cache': (state) => {
+        state.cache = {}
       }
     },
   })
