7b5349f25059d8ee469dcd88b89e2f170a9c5c7e
Jonathan Smith <jonathan@peoplesized.com>
2017-06-03 23:34:44 +0200

    fixed under-caching of instances and over-caching of geo

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -4,10 +4,14 @@ module.exports = {
   maximumFileSizeToCacheInBytes: 14194304,
   navigateFallback: 'index.html',
   importScripts: ['offline-analytics.js'],
-  staticFileGlobsIgnorePatterns: [/static\/@(geo|instances|structure_sample)\/?(?:[^\/]+\/?)*$/],
+  staticFileGlobsIgnorePatterns: [/\/static\/(geo|instances|structure_samples)\/?(?:[^\/]+\/?)*$/],
   runtimeCaching: [
     {
-      urlPattern: /static\/(geo|instances|structure_sample)\/?(?:[^\/]+\/?)*$/,
+      urlPattern: /\/static\/(geo|instances|structure_samples)\/?(?:[^\/]+\/?)*$/,
+      handler: 'cacheFirst'
+    },
+    {
+      urlPattern: '/^https\:\/\/douma-api\.herokuapp.com\/v3',
       handler: 'cacheFirst'
     }
   ],
