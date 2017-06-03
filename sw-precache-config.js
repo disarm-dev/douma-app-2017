2965efca975dd8fec5adb4814a2b0d2b8afc91b3
Jonathan Smith <jonathan@peoplesized.com>
2017-06-03 22:56:46 +0200

    restore caching and offline function

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -4,10 +4,10 @@ module.exports = {
   maximumFileSizeToCacheInBytes: 14194304,
   navigateFallback: 'index.html',
   importScripts: ['offline-analytics.js'],
-  staticFileGlobsIgnorePatterns: [/static\/(instances|geo|structure_sample).*/],
+  staticFileGlobsIgnorePatterns: [/static\/@(geo|instances|structure_sample)\/?(?:[^\/]+\/?)*$/],
   runtimeCaching: [
     {
-      urlPattern: /static\/(instances|geo|structure_sample).*/,
+      urlPattern: /static\/(geo|instances|structure_sample)\/?(?:[^\/]+\/?)*$/,
       handler: 'cacheFirst'
     }
   ],
