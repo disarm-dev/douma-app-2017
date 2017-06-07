e4bc6c9a7f23aae4d138b5c40d3b5d82579f6367
Jonathan Smith <jonathan@peoplesized.com>
2017-06-07 15:54:13 +0200

    fix mapbox compilation errorπ

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -35,7 +35,8 @@ var webpackConfig = merge(baseWebpackConfig, {
     }),
     new webpack.optimize.UglifyJsPlugin({
       compress: {
-        warnings: false
+        warnings: false,
+        comparisons: false
       },
       sourceMap: true
     }),
