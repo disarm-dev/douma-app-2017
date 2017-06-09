d04bccda85923bbafa5c93401b0ca980fa068a7c
Nicolai Davies <nicolaidavies.nd@gmail.com>
2017-06-09 11:19:11 +0200

    Add calculator

--- src/apps/irs_plan/pages/plan-summary.vue
+++ src/apps/irs_plan/pages/plan-summary.vue
@@ -1,5 +1,14 @@
 <template>
   <div>
+    
+    <div>
+      <h3>Calculator</h3>
+      <div>
+      At a rate of  <input class="slim-input" type="numer" v-model="calculator.structures"/> structures per team per day, with  <input class="slim-input" type="number" v-model="calculator.teams"/> teams this would take {{days_to_spray}} days
+    </div>
+    <br>
+
+    </div>
     <h3>Selected regions:</h3>
     <md-button class='md-raised md-primary' @click.native="download_plan">Download plan</md-button>
     <p>Working with {{selected_target_area_ids.length}} regions, containing in total {{number_of_structures}} structures, YY rooms, ZZ population</p>
@@ -25,6 +34,10 @@
     props: ['edit', 'geodata_ready'],
     data() {
       return {
+        calculator: {
+          structures: 40,
+          teams: 20
+        },
         render_table: false,
         _geodata: {
           all_target_areas: null,
@@ -55,6 +68,10 @@
           return {data, columns}
         }
       },
+      days_to_spray() {
+        let structures_per_day = this.calculator.structures * this.calculator.teams
+        return this.number_of_structures / structures_per_day
+      },
       number_of_structures() {
         let field_name = this.structures_field_name
         return this.selected_target_area_ids.map(id => {
@@ -82,4 +99,8 @@
   }
 </script>
 
-<style></style>
+<style>
+  .slim-input {
+    width: 60px;
+  }
+</style>
