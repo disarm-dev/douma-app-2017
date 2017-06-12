296662e4679bbf4cd1c6f61bd6e6e916e3c64c8a
Nicolai Davies <nicolaidavies.nd@gmail.com>
2017-06-12 17:02:44 +0200

    Get rid of Leaflet from Monitor

--- src/apps/irs_monitor/pages/common/map.vue
+++ src/apps/irs_monitor/pages/common/map.vue
@@ -2,26 +2,33 @@
   <div id="map"></div>
 </template>
 <script>
-  import Leaflet from 'leaflet'
+  import mapboxgl from 'mapbox-gl'
   import TurfHelpers from '@turf/helpers'
   import Translations from '@/lib/translations'
+
   export default {
     props: ['responses', 'denominator'],
     data() {
       return {
         _map: null,
-        _buildings_layer: null
+        _instance_translations: null
       }
     },
     watch: {
-      'responses': 'add_buildings'
+      'responses': 'add_records'
     },
     computed: {
       feature_collection() {
         let points = this.responses.map((response) =>{
           let {latitude, longitude} = response.location.coords
           let point = TurfHelpers.point([longitude, latitude])
-          point.properties = response
+          /* 
+            Mapbox does not support dynamic styling via child properties
+            so trying to decide color from 'form_data.visit_type' won't work
+            so we move all form_data properties to the properties of the geojson feature
+            TODO: @refac Find another way to style features dynamically
+          */
+          point.properties = {...response, ...response.form_data}
           return point
         })
         return TurfHelpers.featureCollection(points)
@@ -31,53 +38,63 @@
       }
     },
     mounted() {
+      this._instance_translations = new Translations[this.instance_config.slug](this.instance_config)
       this.create_map()
     },
     methods: {
       create_map() {
-        this._map = Leaflet.map('map', {
-          tms: true,
-          center: [-26.1288, 31.9892],
-          zoom: 16
+        this._map = new mapboxgl.Map({
+          container: 'map',
+          style: 'mapbox://styles/mapbox/streets-v9',
+          center: [31.9892, -26.1288],
+          zoom: 9
         });
 
-        const url = 'https://api.mapbox.com/styles/v1/onlyjsmith/civ9t5x7e001y2imopb8c7p52/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoib25seWpzbWl0aCIsImEiOiI3R0ZLVGtvIn0.jBTrIysdeJpFhe8s1M_JgA'
-
-        Leaflet.tileLayer(url).addTo(this._map);
-
-        this.add_buildings()
+        this.add_records()
+        this.bind_popup()
       },
-      add_buildings() {
-        if (this.responses.length === 0) return
+      add_records() {
+        this._map.on('load', () => {
+          if (this.responses.length === 0) return
 
-        if (this._buildings_layer) { 
-          this._map.removeLayer(this._buildings_layer)
-          this._buildings_layer = null
-        }
-
-        const instance_translations = new Translations[this.instance_config.slug](this.instance_config)
-
-        this._buildings_layer = L.geoJSON(this.feature_collection, {
-          // onClick: (a, b) => {
-          //   console.log(a,b)
-          // },
-          pointToLayer: (feature, latlng) => {
-            return L.circleMarker(latlng, {radius: 5, fillOpacity: 0.9});
-          },
-          style: (feature, layer) => {
-            const base_style = {fill: false}
-            const instance_style = instance_translations.getMapStyle(feature, layer)
-            return {...instance_style, base_style}
+          if (this._map.getLayer('records')) { 
+            this._map.removeLayer('records')
           }
-        }).bindPopup(function (layer) {
-            return instance_translations.getPopupDescription(layer)
+
+          if (this._map.getSource('records')) { 
+            this._map.removeSource('records')
+          }
+
+          const instance_translations = new Translations[this.instance_config.slug](this.instance_config)
+
+          this._map.addLayer({
+            id: 'records',
+            type: 'circle',
+            source: {
+              type: 'geojson',
+              data: this.feature_collection
+            },
+            paint: {
+              'circle-radius': 10,
+              // TODO: @feature increase size of circle with zoom level, look at example
+              // TOOD: @feature Adjust instance_translations.getMapStyle() for all countries
+              ...instance_translations.getMapStyle()
+            }
+          })
         })
-
-        this._buildings_layer.addTo(this._map)
-
-        this._map.fitBounds(this._buildings_layer.getBounds())
-        
       },
+      bind_popup() {
+        this._map.on('click', (e) => {
+          // TODO: @feature Get popup working
+          // TOOD: @feature Adjust instance_translations.getPopupDescription() for all countries
+          const feature = this._map.queryRenderedFeatures(e.point, {layers: ['records']})[0]
+          
+          new mapboxgl.Popup({closeOnClick: true})
+            .setLngLat(e.lngLat)
+            .setHTML(this._instance_translations.getPopupDescription(feature))
+            .addTo(this._map);
+        })
+      }
     },
     
   }
--- src/lib/translations/swz.translations.js
+++ src/lib/translations/swz.translations.js
@@ -6,20 +6,27 @@ export default class extends Base {
     return super.getTableData(responses, denominator, field_name)
   }
 
-  getMapStyle(feature, layer) {
-    let {visit_type} = feature.properties.form_data
+  getMapStyle() {
+
     return {
-      color: visit_type === 'first_visit' ? 'green' : 'orange',
+      'circle-color': {
+        property: 'visit_type',
+        type: 'categorical',
+        stops: [
+          ['first_visit', '#22ff31'],
+          ['mopup', '#f51506']
+        ]
+      }
     }
   }
 
-  getPopupDescription(layer) {
-    let record = layer.feature.properties
+  getPopupDescription(feature) {
+    let record = feature.properties
     return `
       <p><b>Date:</b> ${record.recorded_on}</p>
       <p><b>Recorded by:</b> ${record.user}</p>
-      <p><b>Visit type:</b> ${record.form_data.visit_type}</p>
-      <p><b>Number of structures:</b> ${record.form_data.number_structures_total}\n</p>
+      <p><b>Visit type:</b> ${record.visit_type}</p>
+      <p><b>Number of structures:</b> ${record.number_structures_total}\n</p>
     `
   }
 }
