7a5ae4e3779147f8c38c3eb38266d2059d0d871d
Nicolai Davies <nicolaidavies.nd@gmail.com>
2017-06-12 10:36:34 +0200

    Change format to human readable format

--- src/lib/validations/nam.validations.js
+++ src/lib/validations/nam.validations.js
@@ -1,35 +1,21 @@
 export default [
   {
     name: 'total_sprayed_structures',
-    fn: (f) => {
-      if (f.sprayable_unsprayed === 'yes') return true
-
-      return f.numbersprayed_ddt + f.numbersprayed_delta == f.number_sprayable
-    },
+    fn: (f) => (f.sprayable_unsprayed === 'yes') ||  (f.numbersprayed_ddt + f.numbersprayed_delta == f.number_sprayable),
     message: 'Total sprayed does not equal `ddt`+ `deltamethrin`.',
     type: "warning",
     relevant_questions: ["number_sprayable", "numbersprayed_ddt", "numbersprayed_delta", "sprayable_unsprayed"]
   },
   {
     name: 'sprayable_unsprayed',
-    fn: (f) => {
-      if (f.sprayable_unsprayed === 'no') return true
-
-      return f.number_sprayable === (f.numbersprayed_ddt + f.numbersprayed_delta + f.number_unsprayed)
-    },
+    fn: (f) => (f.sprayable_unsprayed === 'no') || (f.number_sprayable === (f.numbersprayed_ddt + f.numbersprayed_delta + f.number_unsprayed)),
     message: "Number of structures doesn't add up",
     type: "warning",
     relevant_questions: ["number_sprayable", "sprayable_unsprayed", "numbersprayed_ddt", "numbersprayed_delta", "number_unsprayed"]
   },
   {
     name: 'high_number_of_sprayed',
-    fn: (f) => {
-      if (f.sprayable === 'yes')  {
-        return f.number_sprayable < 20
-      } else {
-        return true
-      }
-    },
+    fn: (f) => (f.sprayable === 'yes') && f.number_sprayable < 20,
     message: "Number of sprayable structures seems high. Please check the value.",
     type: "warning",
     relevant_questions: ["number_sprayable", "sprayable"]
