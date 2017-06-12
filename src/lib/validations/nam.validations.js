16e5735f223d0fbac35f191b7913473d9994a508
Nicolai Davies <nicolaidavies.nd@gmail.com>
2017-06-12 12:39:42 +0200

    Restore original validations

--- src/lib/validations/nam.validations.js
+++ src/lib/validations/nam.validations.js
@@ -1,21 +1,35 @@
 export default [
   {
     name: 'total_sprayed_structures',
-    fn: (f) => (f.sprayable_unsprayed === 'yes') ||  (f.numbersprayed_ddt + f.numbersprayed_delta == f.number_sprayable),
+    fn: (f) => {
+      if (f.sprayable_unsprayed === 'yes') return true
+
+      return f.numbersprayed_ddt + f.numbersprayed_delta == f.number_sprayable
+    },
     message: 'Total sprayed does not equal `ddt`+ `deltamethrin`.',
     type: "warning",
     relevant_questions: ["number_sprayable", "numbersprayed_ddt", "numbersprayed_delta", "sprayable_unsprayed"]
   },
   {
     name: 'sprayable_unsprayed',
-    fn: (f) => (f.sprayable_unsprayed === 'no') || (f.number_sprayable === (f.numbersprayed_ddt + f.numbersprayed_delta + f.number_unsprayed)),
+    fn: (f) => {
+      if (f.sprayable_unsprayed === 'no') return true
+
+      return f.number_sprayable === (f.numbersprayed_ddt + f.numbersprayed_delta + f.number_unsprayed)
+    },
     message: "Number of structures doesn't add up",
     type: "warning",
     relevant_questions: ["number_sprayable", "sprayable_unsprayed", "numbersprayed_ddt", "numbersprayed_delta", "number_unsprayed"]
   },
   {
     name: 'high_number_of_sprayed',
-    fn: (f) => (f.sprayable === 'yes') && f.number_sprayable < 20,
+    fn: (f) => {
+      if (f.sprayable === 'yes')  {
+        return f.number_sprayable < 20
+      } else {
+        return true
+      }
+    },
     message: "Number of sprayable structures seems high. Please check the value.",
     type: "warning",
     relevant_questions: ["number_sprayable", "sprayable"]
--- src/lib/validations/swz.validations.js
+++ src/lib/validations/swz.validations.js
@@ -1,21 +1,32 @@
 export default [
   {
     name: 'more_sprayed_than_total',
-    fn: (f) => f.number_of_structures_sprayed <= f.number_structures_total,
+    fn: (f) => {
+      return f.number_of_structures_sprayed <= f.number_structures_total
+    },
     message: 'There are more structures sprayed than total structures.',
     relevant_questions: ['number_of_structures_sprayed', 'number_structures_total'],
     type: "warning"
   },
   {
     name: 'total_sprayed',
-    fn: (f) => (f.any_structures_unsprayed === 'no') &&  (f.number_structures_total === f.number_of_structures_sprayed),
+    fn: (f) => {
+      if (f.any_structures_unsprayed === 'no') {
+        return  f.number_structures_total === f.number_of_structures_sprayed
+      }
+      return true
+    },
     message: 'Total structures does not equal structures sprayed.',
     relevant_questions: ['number_of_structures_sprayed', 'number_structures_total', 'any_structures_unsprayed'],
     type: "warning"
   },
   {
     name: 'total_sprayed_and_unsprayed',
-    fn: (f) => (f.any_structures_unsprayed === 'yes') && (f.number_structures_total === (f.number_of_structures_sprayed + f.n_rooms_baby + f.n_rooms_food + f.n_rooms_funeral + f.n_rooms_kitchen + f.n_rooms_locked + f.n_rooms_material + f.n_rooms_nobody + f.n_rooms_other + f.n_rooms_patient + f.n_rooms_refused)),
+    fn: (f) => {
+      if (f.any_structures_unsprayed === 'yes') {
+        return  f.number_structures_total === (f.number_of_structures_sprayed + f.n_rooms_baby + f.n_rooms_food + f.n_rooms_funeral + f.n_rooms_kitchen + f.n_rooms_locked + f.n_rooms_material + f.n_rooms_nobody + f.n_rooms_other + f.n_rooms_patient + f.n_rooms_refused)
+      }
+    },
     message: 'Total structures does not equal structures sprayed and number of structures unsprayed.',
     relevant_questions: ['number_of_structures_sprayed', 'number_structures_total', 'any_structures_unsprayed', "n_rooms_baby", "n_rooms_food", "n_rooms_funeral", "n_rooms_kitchen", "n_rooms_locked", "n_rooms_material", "n_rooms_nobody", "n_rooms_other", "n_rooms_patient", "n_rooms_refused"],
     type: "warning"
