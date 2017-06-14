fe27ac96453261a3f947ea087fd3d7c12d85bea6
Jonathan Smith <jonathan@peoplesized.com>
2017-06-14 16:13:05 +0200

    non-working spec

---
+++ test/unit/specs/models/plan.spec.js
@@ -0,0 +1,14 @@
+import '@/models/plan.model.js'
+
+describe('Plan model', () => {
+  it('should be created with valid inputs', () => {
+    const valid = {
+      country: 'bob',
+      selected_target_area_ids: [1,2,3]
+    }
+  })
+
+  it('should not be created with invalid inputs', () => {
+
+  })
+})
