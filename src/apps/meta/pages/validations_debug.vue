d2ade2baa6137d24c88902bbc8a8bdd0be580938
Jonathan Smith <jonathan@peoplesized.com>
2017-06-18 06:21:06 +0200

    start

--- src/apps/meta/pages/validations_debug.vue
+++ src/apps/meta/pages/validations_debug.vue
@@ -1,51 +1,68 @@
 <template>
 	<div class="container">
-	  <md-input-container>
+
+	  <md-input-container v-if="false">
       <label>Upload validations</label>
       <md-file @selected="upload_validations"></md-file>
     </md-input-container>
-    <md-button class="md-raised md-primary" @click.native="download"> Download </md-button>
-    <div v-if="validations_debug">
+
+    <md-button :disabled='true' class="md-raised md-primary" @click.native="download">Download changes</md-button>
+
+    <h3>Current validations:</h3>
+    <div v-if="validations">
       <md-list>
-        <md-list-item v-for="validation in validations_debug" :key="validation.name" @click.native="set_active_validation(validation)">
-           {{validation.name}}
+        <md-list-item v-for="validation in validations" :key="validation.name" @click.native="set_active_validation(validation)">
+          <md-icon>chevron_right</md-icon>
+          <span>{{validation.name}}</span>
         </md-list-item>
       </md-list>
     </div>
 
-    <md-card v-if="active_validation">
-      <md-card-header>
-        <md-card-header-text>
-          <div class="md-title">{{active_validation.name}}</div>
-        </md-card-header-text>
-      </md-card-header>
-      <md-card-content>
-        <md-input-container>
-          <label>Precondition</label>
-          <md-input v-model="active_validation.precondition"></md-input>
-        </md-input-container>
-        <md-input-container>
-          <label>Expression</label>
-          <md-input v-model="active_validation.expression"></md-input>
-        </md-input-container>
-      </md-card-content>
-      <md-card-actions>
-        <md-button class="md-primary md-raised" @click.native="save_validation">Save</md-button>
-        <md-button @click.native="active_validation = null">Close</md-button>
-      </md-card-actions>
-    </md-card>
+    <template v-if="active_validation">
+      <!--EXPRESSION EDITOR-->
+      <md-card>
+        <md-card-header>
+          <md-card-header-text>
+            <div class="md-title">{{active_validation.name}}</div>
+          </md-card-header-text>
+        </md-card-header>
+        <md-card-content>
+          <md-input-container>
+            <label>Precondition</label>
+            <md-input v-model="active_validation.precondition"></md-input>
+          </md-input-container>
+          <md-input-container>
+            <label>Expression</label>
+            <md-input v-model="active_validation.expression"></md-input>
+          </md-input-container>
+        </md-card-content>
+        <md-card-actions>
+          <md-button class="md-primary md-raised" @click.native="save_validation">Save</md-button>
+          <md-button @click.native="active_validation = null">discard</md-button>
+        </md-card-actions>
+      </md-card>
 
-    <md-card v-if="active_validation">
-      <md-card-header>
-        <md-card-header-text>
-          <div class="md-title">Form question names</div>
-          <div class="md-subhead">For reference</div>
-        </md-card-header-text>
-      </md-card-header>
-      <md-card-content>
-        <md-chip v-for="element in form_elements" :key="element.name">{{element.name}}</md-chip>
-      </md-card-content>
-    </md-card>
+      <!--TEST EXPRESSION-->
+      <md-card>
+        <md-card-header>
+          <div class="md-title">Test expression</div>
+        </md-card-header>
+      </md-card>
+
+      <!--FORM ELEMENTS-->
+      <md-card>
+        <md-card-header>
+          <md-card-header-text>
+            <div class="md-title">Form question names</div>
+            <div class="md-subhead">For reference</div>
+          </md-card-header-text>
+        </md-card-header>
+        <md-card-content>
+          <md-chip v-for="element in form_elements" :key="element.name">{{element.name}}</md-chip>
+        </md-card-content>
+      </md-card>
+
+    </template>
 
 	</div>
 </template>
@@ -59,8 +76,11 @@
   export default {
     data() {
       return {
-        raw_validations: [],
-        active_validation: null
+        validations: [],
+        active_validation: null,
+
+        show_form_elements: true,
+        show_test_expression: false
       }
     },
     computed: {
@@ -70,40 +90,21 @@
       form_elements() {
         return elements_array(this.$store.state.instance_config.form)
       },
-      validations() {
-        let warnings = this.raw_validations.find((i) => i.type === 'warnings')
-        if (warnings && warnings.hasOwnProperty('validations_debug') && warnings.validations_debug.length) {
-          warnings = warnings.validations_debug.map((i) => {
-            i.type = "warning"
-            return i
-          })
-        } else {
-          warnings = []
-        }
-
-        let errors = this.raw_validations.find((i) => i.type === 'errors')
-        if (errors && errors.hasOwnProperty('validations_debug') && errors.validations_debug.length) {
-          errors = errors.validations_debug.map((i) => {
-            i.type = "error"
-            return i
-          })
-        } else {
-          errors = []
-        }
-
-        return errors.concat(warnings)
+      active_validation_changed() {
+        let index = this.validations.findIndex((i) => i.name === this.active_validation.name)
+        
       }
     },
-    mounted() {
-      this.raw_validations = require("json-loader!lib/validations/" + this.instance_config.slug  + ".validations_debug.json")
+    created() {
+      this.validations = require("json-loader!lib/validations/" + this.instance_config.slug  + ".validations.json")
     },
     methods: {
       set_active_validation(validation) {
         this.active_validation = validation
       },
       save_validation() {
-        let index = this.validations_debug.findIndex((i) => i.name === this.active_validation.name)
-        this.validations_debug.splice(index, this.active_validation)
+        let index = this.validations.findIndex((i) => i.name === this.active_validation.name)
+        this.validations.splice(index, this.active_validation)
         this.active_validation = null
       },
       upload_validations(e) {
