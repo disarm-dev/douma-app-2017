4c84602a29c9bb8b2300f375f5ee535fdc5bb8f3
Jonathan Smith <jonathan@peoplesized.com>
2017-06-20 22:28:08 +0200

    move survey.css to form (only place it's used), and add static bootstrap extract incl only Form elements

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import './fonts/Roboto.css'
 import './fonts/MaterialIcons.css'
 import 'mapbox-gl/dist/mapbox-gl.css'
 import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css'
-import 'survey-vue/survey.css'
 
 // Imports
 import Vue from 'vue'
