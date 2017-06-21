34eaea14b5275c589cdd4982ac594013c44128b5
Jonathan Smith <jonathan@peoplesized.com>
2017-06-21 17:29:49 +0200

    change default map style

--- src/config/common_config.js
+++ src/config/common_config.js
@@ -11,7 +11,7 @@ export default {
   basemap: {
     // Middle of southern Africa, start point for zooming in
     default: {
-      style: 'mapbox://styles/mapbox/basic-v9',
+      style: 'mapbox://styles/mapbox/streets-v9',
       coords: [22.63977015806131, -25.276453102086563],
       zoom: 4
     }
