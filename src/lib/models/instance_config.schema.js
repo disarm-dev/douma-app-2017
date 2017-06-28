d963798f035b0966ecd5ebcb41ae2980d8ce786f
Jonathan Smith <jonathan@peoplesized.com>
2017-06-28 09:58:45 +0200

    updated schema to match desired one (based on ZWE ideas)^

--- src/lib/models/instance_config.schema.js
+++ src/lib/models/instance_config.schema.js
@@ -54,8 +54,14 @@ export const InstanceConfigSchema = schema({
   slug: String,
   spatial_hierarchy: Array.of_x(1, Infinity, SpatialHierarchySchema),
   // TODO: @fix Don't currently have a way to validate the denominator on spatial_hierarchy - could do as separate property
-  // denominator: {
-  //   '?number_of_structures': String,
-  //   '?number_of_households': String
+  // denominator_units: {
+  //   physical: {
+  //     id_field: String, // e.g. number_of_structures
+  //     enumerable: String // e.g "structure"
+  //   },
+  //   human: {
+  //     id_field: String,
+  //     enumerable: String
+  //   },
   // },
 })
