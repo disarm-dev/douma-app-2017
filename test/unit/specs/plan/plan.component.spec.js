3d93381925e2e46aed876c5fa5579e95b0180b74
Nicolai Davies <nicolaidavies.nd@gmail.com>
2017-06-29 10:00:40 +0200

    bette

--- test/unit/specs/plan/plan.component.spec.js
+++ test/unit/specs/plan/plan.component.spec.js
@@ -1,43 +1,43 @@
 xdescribe('plan.vue', () => {
 
-  xdescribe("create new plan", () => {
-    xit('can load all available spatial_hierarchies', () => {
+  describe("create new plan", () => {
+    it('can load all available spatial_hierarchies', () => {
 
     })
 
-    xit('can display selected spatial_heriarchy', () => {})
+    it('can display selected spatial_heriarchy', () => {})
   })
-  xdescribe("view existing plan", () => {})
-  xdescribe("edit existing plan", () => {})
-  xdescribe("clear existing plan", () => {})
+  describe("view existing plan", () => {})
+  describe("edit existing plan", () => {})
+  describe("clear existing plan", () => {})
 
 
 
-  xit('should get latest plan from remote', () => {})
+  it('should get latest plan from remote', () => {})
 
-  xit('should get village-structure-clusters from remote', () => {})
+  it('should get village-structure-clusters from remote', () => {})
 
-  xit('should calculate the number of days to spray from the inputs given', () => {})
+  it('should calculate the number of days to spray from the inputs given', () => {})
 
-  xit('should calculate the correct risk scale', () => {})
+  it('should calculate the correct risk scale', () => {})
 
-  xit("should set 'unsaved_changes' be true after making a change", () => {})
+  it("should set 'unsaved_changes' be true after making a change", () => {})
 
-  xit('should load remote plan when "Cancel edits"', () => {})
+  it('should load remote plan when "Cancel edits"', () => {})
 
-  xit("should clear the plan when 'Clear Plan' is clicked", () => {})
+  it("should clear the plan when 'Clear Plan' is clicked", () => {})
 
-  xit("should save the plan when 'Save' is clicked", () => {})
+  it("should save the plan when 'Save' is clicked", () => {})
 
-  xit('should show controls when in edit mode', () => {})
+  it('should show controls when in edit mode', () => {})
 
   // MAP
-  xit('should show areas by risk when selected', () => {
+  it('should show areas by risk when selected', () => {
     taregt_ares = [1,2,3,4]
     store = { state: {taregt_ares:}}
   })
 
   // TABLE
-  xit('should display all selected areas in the table', () => {})
+  it('should display all selected areas in the table', () => {})
 
 })
