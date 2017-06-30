9d9a11ab9f9f968e360cb8c7c4ee801bb8566cb6
Nicolai Davies <nicolaidavies.nd@gmail.com>
2017-06-30 09:20:12 +0200

    IRS Tasker working!!!

--- src/apps/irs_tasker/pages/tasker.vue
+++ src/apps/irs_tasker/pages/tasker.vue
@@ -2,7 +2,9 @@
   <div class="container">
     <h1>Assign teams</h1>
     <md-button class="md-accent" @click.native="recreate_assignments_from_plan">Recreate assignments from most recent Plan</md-button>
-    <md-button class="md-primary md-raised" @click.native="save">Save</md-button>
+    
+    <md-button class="md-primary md-raised" :disabled="!assignments.length" @click.native="save">Save</md-button>
+
     <tasker_legend :decorated_teams="decorated_teams" :selected_team_name="selected_team_name" @selected_team="select_team"></tasker_legend>
 
     <tasker_map
@@ -90,11 +92,18 @@
             target.assigned_to_team_name = assignment.team_name
             return target
           })
-          console.log('new_targets', new_targets)
+          
           const plan = {
             ...plan_json,
             targets: new_targets
           }
+
+          // Bump time by 10 seconds to make this plan newer than the old one
+          const new_date = new Date(plan.planned_at)
+          
+          new_date.setSeconds(new_date.getSeconds() + 10)
+
+          plan.planned_at = new_date
 
           // Mongo complains if we try insert a document with an existing ID
           delete plan._id
