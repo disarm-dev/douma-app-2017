af8fee535fbeb41178fbe74920b9f8b50d843c77
Nicolai Davies <nicolaidavies.nd@gmail.com>
2017-07-04 14:59:03 +0200

    Update aggregations for ZWE and BWA

--- src/lib_instances/aggregations/bwa.aggregations.js
+++ src/lib_instances/aggregations/bwa.aggregations.js
@@ -4,38 +4,35 @@ export default {
   },
 
   'number of people in the homestead (<5 yrs)': {
-    numerator_function: (d) => d.ppl_in_hs_under5
+    numerator_expr: "ppl_in_hs_under5"
   },
 
   'number of people in the homestead (>5 yrs)': {
-    numerator_function: (d) => d.ppl_in_hs_above5
+    numerator_expr: "ppl_in_hs_above5"
   },
 
   'number of people in homestead (total)': {
-    numerator_function: (d) => d.ppl_in_hs_above5 + d.ppl_in_hs_under5
+    numerator_expr: "ppl_in_hs_above5 + ppl_in_hs_under5"
   },
 
   'number of buildings visited': {
-    numerator_function: (d) => d.n_buildings
+    numerator_expr: "n_buildings"
   },
 
   'number of rooms visited': {
-    numerator_function: (d) => d.room_count
+    numerator_expr: "room_count"
   },
 
   'number of rooms sprayed (total)': {
-    numerator_function: (d) => d.number_sprayed_ddt + d.number_sprayed_lambdacyhalothrin,
     numerator_expr: "number_sprayed_ddt + number_sprayed_lambdacyhalothrin"
   },
 
   'number of rooms sprayed (DDT)': {
-    numerator_function: (d) => d.number_sprayed_ddt
+    numerator_expr: "number_sprayed_ddt"
   },
 
   'number of rooms sprayed (lambda-cyhalothrin)': {
-    numerator_function: (d) => d.number_sprayed_lambdacyhalothrin
-  },
-
-
+    numerator_expr: "number_sprayed_lambdacyhalothrin"
+  }
 }
 
--- src/lib_instances/aggregations/zwe.aggregations.js
+++ src/lib_instances/aggregations/zwe.aggregations.js
@@ -1,31 +1,29 @@
 export default {
   'homesteads found': {
-    numerator_function: () => 1
+    numerator_expr: "1"
   },
   'structures found': {
-    numerator_function: (d) => d.number_sprayable + d.number_unsprayable
+    numerator_expr: "number_sprayable + number_unsprayable"
   },
   'structures found %': {
-    numerator_function: (d) => d.number_sprayable + d.number_unsprayable,
+    numerator_expr: "number_sprayable + number_unsprayable",
     denominator_field: 'number_of_households'
   },
   "structures sprayed": {
-    numerator_function: (d) => d.numbersprayed_PY + d.numbersprayed_ddt + d.numbersprayed_OP
+    numerator_expr: "numbersprayed_PY + numbersprayed_ddt + numbersprayed_OP"
   },
   'structures sprayed %': {
-    numerator_function: (d) => d.numbersprayed_PY + d.numbersprayed_ddt + d.numbersprayed_OP,
+    numerator_expr: "numbersprayed_PY + numbersprayed_ddt + numbersprayed_OP",
     denominator_field: 'number_of_households'
   },
   'sprayable structures not sprayed': {
-    numerator_function: (d) => (d.number_sprayable - (d.numbersprayed_PY + d.numbersprayed_ddt + d.numbersprayed_OP))
+    numerator_expr: "number_sprayable - (numbersprayed_PY + numbersprayed_ddt + numbersprayed_OP)"
   },
   'sprayable structures not sprayed (refused)': {
-    precondition: (d) => d.reasons_notspraying.includes('refused'),
-    numerator_function: (d) => (d.number_sprayable - (d.numbersprayed_PY + d.numbersprayed_ddt + d.numbersprayed_OP))
+    numerator_expr: "number_sprayable - (numbersprayed_PY + numbersprayed_ddt + numbersprayed_OP)"
   },
   'sprayable structures not sprayed (refused) %': {
-    precondition: (d) => d.reasons_notspraying.includes('refused'),
-    numerator_function: (d) => (d.number_sprayable - (d.numbersprayed_PY + d.numbersprayed_ddt + d.numbersprayed_OP)),
+    numerator_expr: "number_sprayable - (numbersprayed_PY + numbersprayed_ddt + numbersprayed_OP)",
     denominator_field: 'number_of_households'
   }
 }
