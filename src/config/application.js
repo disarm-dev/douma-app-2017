8136ac231d6cd7708c8ffc125ea62755ca5a631f
Jonathan Smith <jonathan@peoplesized.com>
2017-07-05 10:38:26 +0200

    renamed

--- src/config/application.js
+++ src/config/application.js
@@ -20,7 +20,7 @@ Vue.material.registerTheme({
 import DoumaComponent from 'components/app.vue'
 import {create_router} from '../router'
 import {create_store} from '../store'
-import {get_instance_stores_and_routes} from './applets'
+import {get_instance_stores_and_routes} from './applet_stores_and_routes'
 import {instantiate_analytics, set_common_analytics} from 'config/analytics'
 
 export function configure_application (instance_config) {
---
+++ src/config/decorated_applets.js
@@ -0,0 +1,6 @@
+
+
+
+function decorate_applets(applets) {
+
+}
