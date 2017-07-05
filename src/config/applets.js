1e3347ee3990afe51358764b15caed4796ba63ec
Jonathan Smith <jonathan@peoplesized.com>
2017-07-05 09:16:07 +0200

    started

--- src/config/applets.js
+++ src/config/applets.js
@@ -1,17 +1,31 @@
 import common_config from 'config/common_config'
 
 // These are all the stores and routes for all the applets in the douma world
-const applets = {
-  data_wizard: require('apps/data_wizard/index'),
-  meta: require('apps/meta/index'),
-  irs_record_point: require('apps/irs_record_point/index'),
-  irs_monitor: require('apps/irs_monitor/index'),
-  irs_plan: require('apps/irs_plan/index'),
-  irs_tasker: require('apps/irs_tasker/index'),
-}
+// const applets = {
+//   data_wizard: require('apps/data_wizard/index'),
+//   meta: require('apps/meta/index'),
+//   irs_record_point: require('apps/irs_record_point/index'),
+//   irs_monitor: require('apps/irs_monitor/index'),
+//   irs_plan: require('apps/irs_plan/index'),
+//   irs_tasker: require('apps/irs_tasker/index'),
+// }
+
+const applet_requires = require("dir-loader!../apps")
+const applet_names = Object.keys(applet_requires)
+
+let applets = []
+applet_names.forEach(applet_name => {
+  applets[applet_name] = applet_requires[applet_name].src
+})
 
 export const decorated_applets = []
 
+/**
+ * Collect stores and routes for applets ONLY in this instance {stores: {}, routes: []}
+ * Ignores user permissions
+ * @param instance_config
+ * @returns {{stores: {}, routes: Array}}
+ */
 export function get_instance_stores_and_routes(instance_config) {
   let stores_and_routes = {stores: {}, routes: []}
 
