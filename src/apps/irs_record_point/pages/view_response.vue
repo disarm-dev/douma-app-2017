457ae29611fb7c6a36769991da7df8de32dace9c
Jonathan Smith <jonathan@peoplesized.com>
2017-07-05 14:19:06 +0200

    stub page

---
+++ src/apps/irs_record_point/pages/view_response.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p>Here's the form!</p>
+    <p>{{response}}</p>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'view_response',
+    props: ['response'],
+    mounted() {
+    },
+    data() {
+      return {}
+    },
+    methods: {}
+  }
+</script>
+
+<style scoped>
+
+</style>
