d88bf22a7c507e283f1166a6cf0ce1fa7557941e
Jonathan Smith <jonathan@peoplesized.com>
2017-07-05 21:39:19 +0200

    done

--- src/apps/meta/pages/debug.vue
+++ src/apps/meta/pages/debug.vue
@@ -1,22 +1,30 @@
 <template>
-  <md-list>
-    <md-list-item><router-link to="/meta/debug/building"><md-icon>location_city</md-icon><span>building</span></router-link></md-list-item>
-    <md-list-item><router-link to="/meta/debug/validations"><md-icon>playlist_add_check</md-icon><span>validations</span></router-link></md-list-item>
-    <md-list-item><router-link to="/meta/debug/fake_data"><md-icon>flight_takeoff</md-icon><span>fake data</span></router-link></md-list-item>
-    <md-divider class="md-inset"></md-divider>
+  <div>
+    <md-toolbar md-theme="white">
+      <span class="md-title">Testing and debug tools</span>
+    </md-toolbar>
+    <md-list>
+      <!--DATA-->
+      <md-list-item><router-link to="/meta/debug/validations"><md-icon>playlist_add_check</md-icon><span>validations</span></router-link></md-list-item>
+      <md-list-item><router-link to="/meta/debug/fake_data"><md-icon>flight_takeoff</md-icon><span>fake data</span></router-link></md-list-item>
+      <md-list-item @click="log_form_elements"><md-icon>assignment</md-icon><span>form elements (check devtools log)</span></md-list-item>
+      <md-divider class="md-inset"></md-divider>
 
-    <md-list-item><router-link to="/meta/debug/location"><md-icon>my_location</md-icon><span>location</span></router-link></md-list-item>
-    <md-list-item @click="check_geolocation()"><md-icon>location_searching</md-icon><span>check geolocation</span></md-list-item>
-    <md-list-item @click="log_form_elements"><md-icon>assignment</md-icon><span>form elements</span></md-list-item>
-    <md-list-item href="/3rdpartylicenses.txt"><md-icon>library_books</md-icon><span>licenses</span></md-list-item>
-    <md-divider class="md-inset"></md-divider>
+      <!-- LOCATION -->
+      <md-list-item><router-link to="/meta/debug/building"><md-icon>location_city</md-icon><span>building hunter</span></router-link></md-list-item>
+      <md-list-item><router-link to="/meta/debug/location"><md-icon>my_location</md-icon><span>location test</span></router-link></md-list-item>
+      <md-list-item @click="check_geolocation()"><md-icon>location_searching</md-icon><span>check geolocation</span></md-list-item>
+      <md-divider class="md-inset"></md-divider>
 
-    <md-list-item @click="clear_local_storage"><md-icon class="md-warn">delete_forever</md-icon><span>clear local storage</span></md-list-item>
+      <!-- LEGAL -->
+      <md-list-item href="/3rdpartylicenses.txt"><md-icon>library_books</md-icon><span>licenses</span></md-list-item>
+      <md-divider class="md-inset"></md-divider>
 
+      <!-- CLEARING THINGS-->
+      <md-list-item @click="clear_local_storage"><md-icon class="md-warn">delete_forever</md-icon><span>clear local storage</span></md-list-item>
 
-    <router-view></router-view>
-
-  </md-list>
+    </md-list>
+  </div>
 </template>
 
 <script>
