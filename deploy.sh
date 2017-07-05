2529cc1457a333bb613a260bf75495cc7e1dca8f
Jonathan Smith <jonathan@peoplesized.com>
2017-07-05 20:21:31 +0200

    deploy adds webhook

--- deploy.sh
+++ deploy.sh
@@ -6,3 +6,9 @@ npm run build
 rm -rf serve/
 mv dist/ serve/
 echo "Built for:" $(git log --oneline -n 1)
+
+echo $(git log --oneline -n 1) |  xargs -I % curl -X "POST" "https://hooks.slack.com/services/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" \
+     -H "Content-Type: application/json; charset=utf-8" \
+     -d $'{
+  "text": "'%I'"
+}'
