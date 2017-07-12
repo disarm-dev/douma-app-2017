712377e928e2a3041d7caea621392fb8daf15877
Jonathan Smith <jonathan@peoplesized.com>
2017-07-12 17:20:28 +0200

    include release in Raven config

--- src/config/error-tracking.js
+++ src/config/error-tracking.js
@@ -7,7 +7,9 @@ const configure_error_tracking = ()=> {
 // Keep track of Errors
   if (process.env.NODE_ENV !== 'development') {
     Raven
-      .config('https://05f42524abca4b84ba7a9b9d05fb620a@sentry.io/134727')
+      .config('https://05f42524abca4b84ba7a9b9d05fb620a@sentry.io/134727', {
+        release: COMMIT_HASH
+      })
       .addPlugin(RavenVue, Vue)
       .install()
     Raven.setExtraContext({
