ea9ab8a2d7a52cff0f591c903ae9b52e64cca2db
Nicolai Davies <nicolaidavies.nd@gmail.com>
2017-07-13 11:56:07 +0200

    filter_area_ids is not optional

--- src/lib/models/plan.schema.js
+++ src/lib/models/plan.schema.js
@@ -3,7 +3,7 @@ import schema from 'js-schema'
 import {TargetSchema} from './target.schema.js'
 
 export const PlanSchema = schema({
-  '?focus_filter_area': [Object, null],
+  focus_filter_area: [Object, null],
   country: String, // e.g. 'swz'
   targets: Array.of_x(0, Infinity, TargetSchema)
 })
