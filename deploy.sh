abed3212fb62c9ef0b09a87a8062aa00d3c684ef
Jonathan Smith <jonathan@peoplesized.com>
2017-07-14 23:52:39 +0200

    remove single/double quotes from git commit/tag/messageπ for deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ rm -rf serve/
 mv dist/ serve/
 echo "Built for:" $(git log --oneline -n 1)
 
-echo $(git log --oneline -n 1) $(git describe) |  xargs -I % curl -X "POST" "https://hooks.slack.com/services/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" \
+echo $(git log --oneline -n 1) $(git describe) | tr -d \'\" | xargs -I % curl -X "POST" "https://hooks.slack.com/services/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" \
      -H "Content-Type: application/json; charset=utf-8" \
      -d $'{
   "text": "'%I'"
