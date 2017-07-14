fe96fcadcc63c52264eb9755869e03562b70ff11
Nicolai Davies <nicolaidavies.nd@gmail.com>
2017-07-14 17:42:05 +0200

    fix plan map

--- src/apps/irs_plan/pages/plan-map.vue
+++ src/apps/irs_plan/pages/plan-map.vue
@@ -290,6 +290,11 @@
           this._map.removeLayer('selected_filter_area')
         if (this._map.getSource('selected_filter_area'))
           this._map.removeSource('selected_filter_area')
+        if (this._map.getSource('area_labels'))
+          this._map.removeSource('area_labels')
+        if (this._map.getLayer('area_labels'))
+          this._map.removeLayer('area_labels')
+
       },
       redraw_target_areas() {
         if (this.geodata_ready && this._map.loaded()) {
