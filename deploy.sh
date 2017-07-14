e1a855ce5c42b269a3bed562e6401527d605c037
Jonathan Smith <jonathan@peoplesized.com>
2017-07-15 00:08:17 +0200

    more better again

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,14 @@ npm install
 npm run build
 rm -rf serve/
 mv dist/ serve/
-echo "Built for:" $(git log --oneline -n 1)
 
-echo $(git rev-parse --abbrev-ref HEAD) $(git log --oneline -n 1) $(git describe) | tr -d \'\" | xargs -I % curl -X "POST" "https://hooks.slack.com/services/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" \
+message=$(git rev-parse --abbrev-ref HEAD)": "
+message=$message" "$(git describe)
+message=$message"    [commit: "$(git log --oneline -n 1)"]"
+echo "Built $message"
+
+echo $message | tr -d \'\" | xargs -I % curl -X "POST" "https://hooks.slack.com/services/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" \
      -H "Content-Type: application/json; charset=utf-8" \
      -d $'{
   "text": "'%I'"
 }'
-
