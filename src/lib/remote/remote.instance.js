d55ea0d37251186041f02f2121fbda65abbc778d
Jonathan Smith <jonathan@peoplesized.com>
2017-07-16 22:12:19 +0200

    restore original url

--- src/lib/remote/remote.instance.js
+++ src/lib/remote/remote.instance.js
@@ -8,8 +8,8 @@ import {standard_handler} from './remote.standard-handler.js'
 export const get_instance_file = (slug, type) => {
   if (!slug || !type) throw new Error(`Need both slug (${slug}) and type (${type}) to get an instance file`)
 
-  // const url =` http://disarm-instance-config.storage.googleapis.com/${slug}/config/${slug}.${type}.json`
-  const url = `https://storage.googleapis.com/disarm-instance-config/${slug}/config/${slug}.${type}.json`
+  const url = `/static/instances/${slug}/config/${slug}.${type}.json`
+  // const url = `https://storage.googleapis.com/disarm-instance-config/${slug}/config/${slug}.${type}.json`
 
   let options = {
     timeout: 20000
