7436f669f6a0b70f3e5787bc0b313b99801b553f
Jonathan Smith <jonathan@peoplesized.com>
2017-07-16 19:04:51 +0200

    no more lib_instances

--- src/lib_instances/presenters/index.js
+++
@@ -1,6 +0,0 @@
-import bwa from './bwa.presenters.js'
-import nam from './nam.presenters.js'
-import swz from './swz.presenters.js'
-import zwe from './zwe.presenters.js'
-
-export default {bwa, nam, swz, zwe}
