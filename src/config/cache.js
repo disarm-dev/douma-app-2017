672869debc00ad3209194f2cfe4cdccfc68e209f
Jonathan Smith <jonathan@peoplesized.com>
2017-07-16 18:26:05 +0200

    let's keep the 'cache' file as simple as possible.

--- src/config/cache.js
+++ src/config/cache.js
@@ -3,11 +3,4 @@ export default {
     // all_target_areas: null, // FeatureCollection
     // clusters: null // FeatureCollection
   },
-  // form: {}, // DONE already gets added to instance_config
-
-
-  location_selector: [], // should have config in instance_config, then create on the fly (and cache)
-  aggregations: [], // index.js needs this
-  presenters: [], // need to be converted, then index.js written, then it will need this
-  validations: [], // index.js needs this
 }
