123bf12895f31de311b30295542c8159754efa86
Jonathan Smith <jonathan@peoplesized.com>
2017-07-16 16:50:17 +0200

    spaces

--- src/apps/meta/pages/check_data_status.vue
+++ src/apps/meta/pages/check_data_status.vue
@@ -2,15 +2,17 @@
   <div>
     Checking status of data...
     <md-list>
-      <span>Record</span>
       <md-list-item><span>instance_config</span></md-list-item>
       <md-list-item><span>form</span></md-list-item>
+
       <md-list-item><span>geodata (lots!)</span></md-list-item>
       <md-list-item><span>location_selector</span></md-list-item>
+
       <md-list-item><span>validations</span></md-list-item>
       <md-list-item><span>aggregations</span></md-list-item>
       <md-list-item><span>fake_form_data (not yet in separate files)</span></md-list-item>
     </md-list>
+
     <md-button @click.native="log_form_elements"><md-icon>assignment</md-icon>log form fields</md-button>
   </div>
 </template>
