888ae497ff042d0cabb2a09f824336d683db9de0
Jonathan Smith <jonathan@peoplesized.com>
2017-07-17 10:27:19 +0200

    comment, remove extra bits

--- src/lib/geodata/generate_location_selection.js
+++ src/lib/geodata/generate_location_selection.js
@@ -4,40 +4,26 @@ import cache from 'config/cache'
 import {geodata_valid} from 'lib/geodata/geodata.valid'
 import {get_all_spatial_hierarchy_levels} from './spatial_hierarchy_helper'
 
-
 export const generate_location_selection = () => {
   if (!geodata_valid()) throw new Error('Invalid geodata')
 
+  // Find the level with a `group_by_field`
   const all_levels = get_all_spatial_hierarchy_levels()
-
   const location_selection_level = all_levels.find(level => {
-    return (get(level, 'group_by_level', false) || get(level, 'group_by_field', false))
+    return get(level, 'group_by_field', false)
   })
-
   if (!location_selection_level) throw new Error('Cannot find a named location selection level')
 
-  if (get(location_selection_level, 'group_by_level')) {
-    return categorise_by_other_level(location_selection_level)
-  } else  if (get(location_selection_level, 'group_by_field', false)) {
-    return categorise_by_field(location_selection_level)
-  }
-}
-
-function categorise_by_field(location_selection_level) {
-  const category_field = location_selection_level.group_by_field
+  // Get all the features from cache.geodata and extract the id and name, attach the category_field
   const level_features = cache.geodata[location_selection_level.name].features
 
   const categorised = level_features.map(feature => {
     return {
       id: feature.properties.__disarm_geo_id,
       name: feature.properties.__disarm_geo_name,
-      category: feature.properties[category_field]
+      category: feature.properties[location_selection_level.group_by_field]
     }
   })
 
   return categorised
-}
-
-function categorise_by_other_level(location_selection_level) {
-
 }
