4479b5738a0890792ca8a168ee2e53044ddc3610
Jonathan Smith <jonathan@peoplesized.com>
2017-07-17 12:11:12 +0200

    add   get_record_location_selection, and refactor to add spatial_hierarchy_cache

--- src/lib/geodata/spatial_hierarchy_helper.js
+++ src/lib/geodata/spatial_hierarchy_helper.js
@@ -4,9 +4,11 @@
  * @returns {*|string}
  */
 let instance_config_cache = null
+let spatial_hierarchy_cache = null
 
 const configure_spatial_helpers = (instance_config) => {
   instance_config_cache = instance_config
+  spatial_hierarchy_cache = instance_config.spatial_hierarchy
 }
 
 /**
@@ -14,8 +16,8 @@ const configure_spatial_helpers = (instance_config) => {
  * @returns {*|string}
  */
 const get_planning_level_id_field = () => {
-  const planning_level_name = instance_config_cache.spatial_hierarchy.markers.planning_level_name
-  const planning_level = instance_config_cache.spatial_hierarchy.levels.find(sp => sp.name === planning_level_name)
+  const planning_level_name = spatial_hierarchy_cache.markers.planning_level_name
+  const planning_level = spatial_hierarchy_cache.levels.find(sp => sp.name === planning_level_name)
 
   if (planning_level && planning_level.hasOwnProperty('field_name')) {
     return planning_level.field_name
@@ -28,48 +30,53 @@ const get_planning_level_id_field = () => {
  * Something like 'villages' for NAM for 'localities' for SWZ
  */
 const get_planning_level_name = () => {
-  const planning_level_name = instance_config_cache.spatial_hierarchy.markers.planning_level_name // e.g. villages for NAM
-  const planning_level = instance_config_cache.spatial_hierarchy.levels.find(sp => sp.name === planning_level_name)
+  const planning_level_name = spatial_hierarchy_cache.markers.planning_level_name // e.g. villages for NAM
+  const planning_level = spatial_hierarchy_cache.levels.find(sp => sp.name === planning_level_name)
   return planning_level.name
 }
 
 const get_planning_level_display_name = () => {
-  const planning_level_name = instance_config_cache.spatial_hierarchy.markers.planning_level_name // e.g. villages for NAM
-  const planning_level = instance_config_cache.spatial_hierarchy.levels.find(sp => sp.name === planning_level_name)
+  const planning_level_name = spatial_hierarchy_cache.markers.planning_level_name // e.g. villages for NAM
+  const planning_level = spatial_hierarchy_cache.levels.find(sp => sp.name === planning_level_name)
   return planning_level.display_field_name
 }
 
 const get_field_name_for_level = (level_name) => {
-  const level = instance_config_cache.spatial_hierarchy.levels.find(level => level.name === level_name)
+  const level = spatial_hierarchy_cache.levels.find(level => level.name === level_name)
   if (!level) throw new Error(`Cannot find level ${level_name} in instance_config.spatial_hierarchy.levels`)
   return level.field_name
 }
 
 const get_display_name_for_level = (level_name) => {
-  const level = instance_config_cache.spatial_hierarchy.levels.find(level => level.name === level_name)
+  const level = spatial_hierarchy_cache.levels.find(level => level.name === level_name)
   if (!level) throw new Error(`Cannot find level ${level_name} in instance_config.spatial_hierarchy.levels`)
   return level.display_field_name
 }
 
 
 const get_denominator_fields = () => {
-  return instance_config_cache.spatial_hierarchy.markers.denominator_fields
+  return spatial_hierarchy_cache.markers.denominator_fields
 }
 
 const get_all_spatial_hierarchy_levels = () => {
-  return instance_config_cache.spatial_hierarchy.levels
+  return spatial_hierarchy_cache.levels
 }
 
 const get_all_spatial_hierarchy_level_names = () => {
-  return instance_config_cache.spatial_hierarchy.levels.map(level => level.name)
+  return spatial_hierarchy_cache.levels.map(level => level.name)
 }
 
 const get_level_by_name = (name) => {
-  return instance_config_cache.spatial_hierarchy.levels.find(level => level.name === name)
+  return spatial_hierarchy_cache.levels.find(level => level.name === name)
 }
 
 const get_top_level_hierarchy = () => {
-  return instance_config_cache.spatial_hierarchy.levels[0]
+  return spatial_hierarchy_cache.levels[0]
+}
+
+const get_record_location_selection = () => {
+  const record_location_selection_level_name = spatial_hierarchy_cache.markers.record_location_selection_level_name
+  return instance_config_cache.location_selection[record_location_selection_level_name]
 }
 
 
@@ -79,8 +86,8 @@ const get_top_level_hierarchy = () => {
  * @returns a level {fieldname, name} or `false`
  */
 const get_next_level_up_from_planning_level = () => {
-  const planning_level_name = instance_config_cache.spatial_hierarchy.markers.planning_level_name
-  const levels = instance_config_cache.spatial_hierarchy.levels
+  const planning_level_name = spatial_hierarchy_cache.markers.planning_level_name
+  const levels = spatial_hierarchy_cache.levels
 
   const index = levels.findIndex(l => l.name === planning_level_name)
 
@@ -92,8 +99,8 @@ const get_next_level_up_from_planning_level = () => {
  * @returns a level {fieldname, name} or `false`
  */
 const get_next_level_down_from_planning_level = () => {
-  const planning_level_name = instance_config_cache.spatial_hierarchy.markers.planning_level_name
-  const levels = instance_config_cache.spatial_hierarchy.levels
+  const planning_level_name = spatial_hierarchy_cache.markers.planning_level_name
+  const levels = spatial_hierarchy_cache.levels
 
   const index = levels.findIndex(l => l.name === planning_level_name)
 
@@ -112,6 +119,7 @@ export {
   get_all_spatial_hierarchy_levels,
   get_all_spatial_hierarchy_level_names,
   get_level_by_name,
+  get_record_location_selection,
 
   get_next_level_up_from_planning_level,
   get_next_level_down_from_planning_level,
