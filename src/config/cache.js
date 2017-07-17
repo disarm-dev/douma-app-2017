114d76517cd4d391a31629cef415f30bb3bdb87d
Jonathan Smith <jonathan@peoplesized.com>
2017-07-17 09:00:02 +0200

    global debug for cache (geodata)

--- src/config/cache.js
+++ src/config/cache.js
@@ -1,6 +1,10 @@
-export default {
+const cache = {
   geodata: {
     // all_target_areas: null, // FeatureCollection
     // clusters: null // FeatureCollection
   },
 }
+// TODO: @debug Remote this global when we no longer need our training wheels
+window.__disarm_debug_cache = cache
+
+export default cache
