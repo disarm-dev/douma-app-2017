ae4eb3edbbbc531d64f40c7c6e7ae5482982565e
Jonathan Smith <jonathan@peoplesized.com>
2017-07-17 12:12:14 +0200

    rejig boot

--- src/config/application.js
+++ src/config/application.js
@@ -34,26 +34,41 @@ import {configure_standard_handler} from 'lib/remote/remote.standard-handler'
 import {try_reconnect} from 'lib/remote/remote.standard-handler'
 
 export function create_and_launch_application (instance_config) {
-  // Configure spatial_helpers to use instance_config
-  configure_spatial_helpers(instance_config)
-  console.log('TODO: @idea 🤔 Can we start getting the tile-cover thing going here, to cache all needed vector tiles? https://github.com/mapbox/tile-cover')
+
+  // CREATE REQUIRED OBJECTS FOR APP (store AND router)
 
   // Collect stores and routes for applets ONLY in this instance {stores: {}, routes: []}
   // Ignores user permissions
   const instance_applets_stores_and_routes = get_instance_stores_and_routes(instance_config)
 
-  //Trigger a ping to API, for lots of reasons, mostly that the API seems to take ages to wake up
-  try_reconnect()
-
-  // Make Vuex#$store and a Vue#$router from what you got
+  // Make Vuex#$store and replace rehydrated (by vuex-persistedstate) instance_config with received instance_config
+  // (Required for the app)
   const store = create_store(instance_config, instance_applets_stores_and_routes.stores)
+  store.commit('root:set_instance_config', instance_config)
+
+  // Create Vue#$router from what you got
+  // (Required for the app)
   const router = create_router(instance_applets_stores_and_routes.routes, store)
 
+
+  // BEFORE VUE APP IS CREATED (USING store OR router)
+
+  // Configure spatial_helpers to use instance_config
+  configure_spatial_helpers(instance_config)
+  console.log('TODO: @idea 🤔 Can we start getting the tile-cover thing going here, to cache all needed vector tiles? https://github.com/mapbox/tile-cover')
+
   // Configure standard_handler for remote requests
+  // Also trigger a ping to API, for lots of reasons, mostly that the API seems to take ages to wake up
+  // (Loads of components use network requests when they are created/mounted)
+  try_reconnect()
   configure_standard_handler(store)
 
   // Analytics 1/2: instantiate analytics before you create the application
+  // (Vue injects $ga in every component)
   instantiate_analytics(router)
+
+
+  // CREATE VUE APP
 
   // Instantiate Vue app with store and router
   const douma_app = new Vue({
@@ -63,7 +78,11 @@ export function create_and_launch_application (instance_config) {
     render: createElement => createElement(DoumaComponent),
   })
 
+
+  // AFTER VUE APP IS CREATED
+
   // Analytics 2/2: set common properties (e.g. user) for every event
+  // (App needs to be running to send the first requests setting user props, etc)
   set_common_analytics(douma_app)
 
   return douma_app
