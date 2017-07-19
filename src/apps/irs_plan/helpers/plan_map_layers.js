cb56e088c46239b14f2cf137cade2fcd43bcd7a5
Jonathan Smith <jonathan@peoplesized.com>
2017-07-19 16:30:11 +0200

    plan map layers

---
+++ src/apps/irs_plan/helpers/plan_map_layers.js
@@ -0,0 +1,30 @@
+export const layer_definitions = {
+  selected_areas: {
+    bulk_selected: {
+      name: 'Selected (by risk)'
+      colour: '#df8ad9'
+    },
+    bulk_unselected: {
+      name: 'Unselected (by risk)',
+      colour: '#fff'
+    },
+    selected: {
+      name: 'Selected (by click)',
+      colour: '#de27da'
+    },
+    unselected: {
+      name: 'Unselected (by click)',
+      colour: '#fff'
+    },
+  },
+
+  selected_filter_area: {
+    name: 'Selected filter area',
+    colour: '#f400d7'
+  },
+
+  lowest_spatial_level: {
+    name: 'Lowest spatial level',
+    colour: '#ff8a21'
+  },
+}
