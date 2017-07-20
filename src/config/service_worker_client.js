f7eda894eccdfc44eb3bc17ee9cab83b3a3ca49c
Jonathan Smith <jonathan@peoplesized.com>
2017-07-20 16:07:44 +0200

    rewrite service-worker client

--- src/config/service_worker_client.js
+++ src/config/service_worker_client.js
@@ -1,60 +1,36 @@
+import pubsubcache from 'lib/helpers/pubsubcache'
+
 //
 // SERVICE WORKER
 //
 
-export function configure_service_worker (DOUMA) {
-  if (!DOUMA_PRODUCTION_MODE) return console.warn("DOUMA ServiceWorker disabled in development")
-
-  if (DOUMA_PRODUCTION_MODE && 'serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js').then(function(reg) {
-      reg.onupdatefound = function() {
-        var installingWorker = reg.installing;
-        installingWorker.onstatechange = function() {
-          switch (installingWorker.state) {
-            case 'installed':
-              if (navigator.serviceWorker.controller) {
-                // At this point, the old content will have been purged and the fresh content will
-                // have been added to the cache.
-                // It's the perfect time to display a "New content is available; please refresh."
-                // message in the page's interface.
-                DOUMA.$store.commit("root:set_sw_message", {
-                  title: `DiSARM updated to ${VERSION_COMMIT_HASH_SHORT}`,
-                  message: "Please click 'Refresh' to reload page and start using the newer version. " +
-                    "You may lose unsaved work. Click 'Cancel' and save if you prefer "
-                })
-              } else {
-                // At this point, everything has been precached.
-                // It's the perfect time to display a "Content is cached for offline use." message.
-                DOUMA.$store.commit("root:set_snackbar", {message: "DiSARM can now go offline for 'IRS Record'"})
-              }
-              break;
-
-            case 'redundant':
-              console.error('The installing service worker became redundant.');
-              break;
-          }
-        };
-      };
-    }).catch(function(e) {
-      console.error('Error during service worker registration:', e);
-    });
-
-    window.addEventListener('beforeinstallprompt', function(e) {
-      // beforeinstallprompt Event fired
-
-      // e.userChoice will return a Promise.
-      // For more details read: https://developers.google.com/web/fundamentals/getting-started/primers/promises
-      e.userChoice.then(function(choiceResult) {
-
-        console.log(choiceResult.outcome);
-
-        if(choiceResult.outcome == 'dismissed') {
-          console.log('User cancelled home screen install');
-        }
-        else {
-          console.log('User added to home screen');
-        }
-      });
-    });
+export function configure_service_worker () {
+  if (!DOUMA_PRODUCTION_MODE) {
+    console.warn("DOUMA ServiceWorker disabled in development")
+    return Promise.resolve()
   }
+
+  if (!'serviceWorker' in navigator) {
+    console.log('ServiceWorker not available in this browser')
+    return Promise.resolve()
+  }
+
+  return new Promise((resolve, reject) => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then((registration) => {
+        pubsubcache.publish('service_worker/onstatechange', '...waiting...')
+        // parsed, installing, installed, activating, activated, and redundant
+        registration.onupdatefound = () => {
+          var installingWorker = registration.installing
+
+          installingWorker.onstatechange = () => {
+            console.log('service_worker/onstatechange', installingWorker.state)
+            pubsubcache.publish('service_worker/onstatechange', installingWorker.state)
+          }
+        }
+      }).catch(e => {
+        console.error(e)
+        reject(e)
+      })
+  })
 }
