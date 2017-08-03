f872509213d134a6500156412518a63f9428d4dc
Jonathan Smith <jonathan@peoplesized.com>
2017-08-03 11:33:30 -0700

    tweak

--- src/apps/data_wizard/pages/wizard.vue
+++ src/apps/data_wizard/pages/wizard.vue
@@ -1,11 +1,13 @@
 <template>
-  <md-stepper md-vertical class="container" @change='change' @completed="completed">
-    <md-step v-for='{title, description, remote_source} in steps' :key='{title}' :md-label="title" :md-message="description">
-      <p><md-icon class='step-icon'>extension</md-icon> {{remote_source}}</p>
-
-      <p><md-icon class='step-icon'>file_upload</md-icon>Upload your own data which matches this <router-link to=''>schema</router-link></p>
-    </md-step>
-  </md-stepper>
+  <div class="container" >
+    <md-stepper md-vertical @change='change' @completed="completed">
+      <md-step v-for='{title, description, remote_source} in steps' :key='{title}' :md-label="title">
+        <md-radio v-model="test1" md-value="1">Use default sources (MAP, AfriPop, etc)</md-radio>
+        <md-radio v-model="test1" md-value="2">Upload your own data</md-radio>
+      </md-step>
+    </md-stepper>
+    <md-button class="md-raised md-warn">Create DiSARM</md-button>
+  </div>
 </template>
 
 <script>
@@ -16,61 +18,16 @@
     },
     data() {
       return {
+        test1: true,
         steps: [
           {
-            title: 'datastore',
-            description: 'Storage for form responses, team assignments, users+permissions',
-            remote_source: 'mongoDB somewhere'
+            title: 'Integrate data',
           },
           {
-            title: 'instance-area',
-            description: 'Area of interested: search for country, region, etc.',
-            remote_source: 'DiSARM Region selection Tool / Generic bounding box'
+            title: 'Select analyses',
           },
           {
-            title: 'enumeration-structures',
-            description: 'Smallest targeting unit',
-            remote_source: 'OSM Buildings extract'
-          },
-          {
-            title: 'sprayable-structures',
-            description: 'Determine which structures are sprayable',
-            remote_source: 'DiSARM Sprayable Buildings API'
-          },
-          {
-            title: 'risk',
-            description: 'Identify highest priority areas',
-            remote_source: 'MAP Map / DiSARM Automated Risk Map API'
-          },
-          {
-            title: 'population',
-            description: 'Human population layer',
-            remote_source: 'WorldPop'
-          },
-          {
-            title: 'admin_levels',
-            description: 'Regions, wards, districts, etc',
-            remote_source: 'GADM or Natural Earth'
-          },
-          {
-            title: 'planning_level',
-            description: 'Operational planning level - either select from admin levels, or cluster something',
-            remote_source: 'DiSARM Enumeration Clustering API'
-          },
-          {
-            title: 'form',
-            description: 'Data collection form',
-            remote_source: 'DiSARM Survey generator Tool (based on SurveyJS)'
-          },
-          {
-            title: 'validations',
-            description: 'Describe checks to ensure good quality data collection',
-            remote_source: 'DiSARM Validation Tool'
-          },
-          {
-            title: 'aggregations',
-            description: 'Describe how you want to aggregate your data (based on ',
-            remote_source: 'DiSARM Aggregation Tool'
+            title: 'Configure applications',
           },
         ]
       }
