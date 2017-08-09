ca489b6003ae8fc0d8e201092b202b72a280c251
Nicolai Davies <nicolaidavies.nd@gmail.com>
2017-08-09 10:22:58 +0200

    Add clear data page

---
+++ src/apps/meta/pages/clear_data.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="container">
+    <md-list>
+      <md-list-item>
+        <md-icon>delete</md-icon>
+        <span>Clear data</span>
+
+        <md-list-expand>
+          <md-list-item @click="clear_geodata"><md-icon>language</md-icon><span>clear geodata</span><md-icon v-if='geodata_cleared' class="md-primary">check</md-icon></md-list-item>
+          <md-list-item v-for="applet in applets" :key="applet" @click="clear_applet_storage(applet)">
+            <md-icon>delete</md-icon><span>clear storage for {{applet}}</span>
+          </md-list-item>
+          <md-list-item @click="clear_local_storage"><md-icon class="md-warn">delete_forever</md-icon><span>clear local storage (wipes all records, data, etc)</span></md-list-item>
+        </md-list-expand>
+      </md-list-item>
+    </md-list>
+  </div>
+</template>
+
+<script>
+  import cache from 'config/cache.js'
+  
+  export default {
+    name: 'clear_data',
+    data () {
+      return {
+        geodata_cleared: false
+      }
+    },
+    computed: {
+      applets() {
+        return get(this.$store.state.meta, 'user.allowed_apps.read', [])
+      },
+    },
+    methods: {
+      clear_geodata() {
+        cache.geodata = {}
+        this.geodata_cleared = true
+      },
+      clear_local_storage() {
+        localStorage.clear()
+        console.log('Cleared localStorage')
+        this.$router.push('/')
+        location.reload()
+      },
+      clear_applet_storage(applet) {
+        console.log('applet', applet)
+        const mutation  = `${applet}/clear_data_storage`
+        this.$store.commit(mutation, {}, {root: true})
+      },
+    }
+  }
+</script>
+
+<style lang="css" scoped>
+</style>
