538857cf387f8f056ca3d456de69af6b386afb9d
Jonathan Smith <jonathan@peoplesized.com>
2017-08-09 21:02:43 -0700

    idea

--- src/lib/remote/remote.authenticate.js
+++ src/lib/remote/remote.authenticate.js
@@ -2,7 +2,10 @@ import {standard_handler, douma_api_root} from './remote.standard-handler.js'
 
 // User authentiction
 export const authenticate = (user) => {
-  let url = douma_api_root + `/auth`
+  if (!DOUMA_PRODUCTION_MODE) {
+    console.log("🚨 Hey, you're in development mode, you really want to auth with a remote service? If I had all the applets, I could fake a user right here for dev use.", user)
+  }
+    let url = douma_api_root + `/auth`
 
   let options = {
     data: {user},
