5587273fd04b58f20b4eac8eeed4ff3708f02815
Nicolai Davies <nicolaidavies.nd@gmail.com>
2017-08-16 15:27:59 +0200

    It's working

--- src/lib/instance_data/decorators.js
+++ src/lib/instance_data/decorators.js
@@ -1,23 +1,58 @@
+import {Parser} from 'expr-eval'
+
 const instance_decorator = (responses, instance_config) => {
-  return responses
-  // insert magic here
   const decorators = instance_config.decorators
 
-  // for decorator_key of decorators {
-  //   const property_name = decorators[decorator_key]
-  //   // more magic in here
-  //   evaluate_decorator(responses, property_name, )
-  // }
+  const decorated_responses = responses.map((response) => {
+    return evaluate_decorators(response, decorators)
+  })
 
-  // return decorated_responses
+  return decorated_responses
 }
 
-// const evaluate_decorator = (responses, property_name, decorator_expressions_array) => {
-//   const key = property_name
-//   const value = decorator_expressions_array
+const evaluate_decorators = (response, decorators) => {
+  const computed_value_names = Object.keys(decorators)
 
-//   return {key: value}
-// }
+  // Loop over the decorators
+  const computed_values = computed_value_names.reduce((result, computed_value_name) => {
+    const decorator_expressions_array = decorators[computed_value_name]
+
+    result[computed_value_name] = evaluate_decorator(response, decorator_expressions_array)
+
+    return result
+  }, {})
+
+
+  // Attach the computed values to the response and return it
+  response.computed = computed_values
+  return response
+}
+
+const evaluate_decorator = (response, decorator_expressions_array) => {
+
+  let result
+
+  // expression is something like {"red": "any_sprayed === true"},
+  for (const expression_definition of decorator_expressions_array) {
+    // possible_value_name is 'red'
+    const possible_value_name = Object.keys(expression_definition)[0]
+
+    // expression is "any_sprayed === true"
+    const expression = expression_definition[possible_value_name]
+
+    const parser = new Parser(expression)
+    
+    const parsed_expresssion = parser.parse(expression)
+
+    const evaluation_result = parsed_expresssion.evaluate(response.form_data)
+    
+    if (evaluation_result) {
+      return possible_value_name
+    }
+  }
+
+  return undefined
+}
 
 
 export default instance_decorator
