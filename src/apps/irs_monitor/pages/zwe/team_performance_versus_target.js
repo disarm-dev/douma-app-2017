9111da4e24c6378e264d254271b0435d58e1c013
Jonathan Smith <jonathan@peoplesized.com>
2017-08-21 15:00:48 +0200

    getting there with first

--- src/apps/irs_monitor/pages/zwe/team_performance_versus_target.js
+++ src/apps/irs_monitor/pages/zwe/team_performance_versus_target.js
@@ -3,48 +3,47 @@ window.n = nest
 
 export default {
   get_data({responses, denominator, aggregations}) {
-    // input_data === filtered_responses
-    // use the aggregator
 
-    // format data into right format for chart
-    const def = [
-      {
-        groupBy: 'team_name',
-        def: {
-          y: {
-            aggregation: 'number_sprayed'
-          },
-          x: {
-            key: 'team_name'
-          },
-          name: 'team_name',
-          type: 'bar'
-        }
-      }
-    ]
-    
+    // const target_data = [
+    //   {
+    //     x: ['Team 2'],
+    //     y: [750],
+    //     name: 'Team 2',
+    //     type: 'bar'
+    //   }, {
+    //     x: ['Team 1'],
+    //     y: [1340],
+    //     name: 'Team 1',
+    //     type: 'bar'
+    //   }
+    // ]
+    //
+
+    //// STARTING HERE
+    let data = []
+
+    // split into series
+    const binned_responses = nest().key('team_name').entries(responses) // e.g. [{key: 'Team a', values: [Responses]}, {key: 'team b', values: [Responses]}]
+
+    for(bin of binned_responses) {
+      // get result of 'structures sprayed' aggregation
 
 
 
+      // const aggregation = (array_of_responses) => {
+      //   return array_of_responses.reduce((acc, val) => {
+      //     return val.form_data.number_sprayed + acc
+      //   }, 0)
+      // }
 
-    const data = [
-      {
-        x: ['Team 2'],
-        y: [750],
-        name: 'Team 2',
+      const value = aggregation(bin.values)
+
+      data.push({
+        x: [bin.key],
+        y: [value],
+        name: bin.key,
         type: 'bar'
-      }, {
-        x: ['Team 1'],
-        y: [1340],
-        name: 'Team 1',
-        type: 'bar'
-      }
-    ]
-
-    const _data = {
-      x: ['Team 1', 'Team 2'],
-      y: [1340, 750],
-      type: 'bar'
+      })
     }
 
     return data
