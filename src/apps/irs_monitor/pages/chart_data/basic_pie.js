23f0fb84e1e71b07334c7f7aedeb2f3d0565daef
Nicolai Davies <nicolaidavies.nd@gmail.com>
2017-08-22 15:06:48 +0200

    Pie chart

--- src/apps/irs_monitor/pages/chart_data/basic_pie.js
+++ src/apps/irs_monitor/pages/chart_data/basic_pie.js
@@ -1,1 +1,23 @@
-basic_pie.js
+import {nest} from 'd3-collection'
+import {ascending} from 'd3-array'
+import get from 'lodash.get'
+import {aggregate_for_pie_chart} from '../../lib/aggregate_series_for_chart'
+
+export default function get_data({responses, denominators, aggregations, options}) {
+    // from configuration/options
+    const defaults = {
+      // options also needs 'bin_by' as a field on the response, can be nested
+      chart_type: 'bar',
+    }
+
+    options = {...defaults, ...options}
+
+    // split/bin into series
+    const binned_responses = nest()
+      .key(f => get(f, options.bin_by))
+      .sortKeys(ascending)
+      .entries(responses)
+
+    return aggregate_for_pie_chart({binned_responses, options, aggregations, denominators})
+  }
+
