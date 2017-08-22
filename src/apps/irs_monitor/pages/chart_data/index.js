454517ce04c1b7709a5ba55c1034fefca9cf1126
Nicolai Davies <nicolaidavies.nd@gmail.com>
2017-08-22 14:59:50 +0200

    Update charts

--- src/apps/irs_monitor/pages/chart_data/index.js
+++ src/apps/irs_monitor/pages/chart_data/index.js
@@ -1,7 +1,9 @@
 import basic_series from './basic_series'
 import time_series from './time_series'
+import basic_pie from './basic_pie'
 
 export default {
   basic_series,
-  time_series
+  time_series,
+  basic_pie
 }
