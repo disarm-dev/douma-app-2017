247d8cf39f21ab9a526eac77b7a15e8ae4354dca
Jonathan Smith <jonathan@peoplesized.com>
2017-09-05 11:42:16 +0200

    stub tests for responses + monitor table/map

---
+++ test/unit/specs/irs_monitor/map-responses.spec.js
@@ -0,0 +1,13 @@
+describe('responses displayed on map', () => {
+  it('creates a row for each unique response.location.selection.name', () => {
+    assert(false, 'unimplemented')
+  })
+
+  it('does not include in rows-to-display any row with a custom location-selection', () => {
+    assert(false, 'unimplemented')
+  })
+
+  it('somehow notifies user that there are responses not shown on map', () => {
+    assert(false, 'unimplemented')
+  })
+})
---
+++ test/unit/specs/irs_monitor/table-responses.spec.js
@@ -0,0 +1,5 @@
+describe('responses displayed in table', () => {
+  it('creates a row for each unique response.location.selection.name', () => {
+    assert(false, 'unimplemented')
+  })
+})
