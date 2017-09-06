c1645f8aec92b832892fa986fa5dbcdd3249d5fc
Nicolai Davies <nicolaidavies.nd@gmail.com>
2017-09-06 10:22:00 +0200

    Fix risk popup

--- src/config/map_layers.js
+++ src/config/map_layers.js
@@ -2,7 +2,7 @@ export const layer_definitions = {
   risk: {
     palette: 'RdYlBu',
     reverse_palette: true,
-    attribute: 'normalised_risk',
+    attribute: 'risk',
     legend_title: "Risk (cases per 1000)"
   },
   default_palette: {
