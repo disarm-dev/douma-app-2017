669b6d0b8edb5139b4fca116368b392fefcccbf8
Nicolai Davies <nicolaidavies.nd@gmail.com>
2017-09-11 15:40:21 +0200

    More passing tests

--- src/lib/models/geodata/geodata.versions.js
+++ src/lib/models/geodata/geodata.versions.js
@@ -1,3 +1,16 @@
-export function check_version(local_version, require_version) {
+/**
+ *
+ * @param {number} local_version
+ * @param {number} required_version
+ * @returns {boolean}
+ */
+export function versions_match(local_version, required_version) {
+
+  const both_versions_are_numbers = Number.isInteger(local_version) && Number.isInteger(required_version)
+
+  if (!both_versions_are_numbers) return false
+
+  if (local_version == required_version) return true
+
   return false
 }
