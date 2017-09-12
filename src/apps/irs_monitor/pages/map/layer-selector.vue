d81a3f87611e60a580db65327553d05abaa67bd1
Jonathan Smith <jonathan@peoplesized.com>
2017-09-12 14:33:20 +0200

    minor refac

--- src/apps/irs_monitor/pages/map/layer-selector.vue
+++ src/apps/irs_monitor/pages/map/layer-selector.vue
@@ -1,21 +1,21 @@
 <template>
-  <div>
-
-    <multiselect
-        :options="layer_options"
-        track-by="id"
-        label="label"
-        @input="set_selected_layer"
-        :value="selected_layer_id"
-    >
-    </multiselect>
-
-  </div>
+  <multiselect
+      :options="layer_options"
+      track-by="id"
+      label="label"
+      @input="set_selected_layer"
+      :value="selected_layer_id"
+  >
+  </multiselect>
 </template>
 
 <script>
-  //import { mapState, mapActions, mapMutations } from 'vuex'
   import Multiselect from 'vue-multiselect'
+
+  const FIXED_OPTIONS = [
+    {id: 'none', label: 'No layer'},
+    {id: 'normalised_risk', label: 'Risk'}
+  ]
 
   export default {
     name: 'layer-selector',
@@ -26,15 +26,9 @@
     },
     computed: {
       layer_options() {
-        const fixed_options = [
-          { id: 'none', label: 'No layer' },
-          { id: 'normalised_risk', label: 'Risk' }
-        ]
-
-        return fixed_options.concat(this.aggregation_names.map(name => {
+        return FIXED_OPTIONS.concat(this.aggregation_names.map(name => {
           return {id: name, label: name}
         }))
-
       },
       selected_layer_id() {
         return this.layer_options.find(o => o.id === this.selected_layer)
