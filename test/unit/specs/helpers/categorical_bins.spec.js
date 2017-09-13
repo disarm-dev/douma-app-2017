618c6e2984f4f9d1f50976c0cfbbd6025af49edf
Nicolai Davies <nicolaidavies.nd@gmail.com>
2017-09-13 11:22:40 +0200

    Test categorical binning

---
+++ test/unit/specs/helpers/categorical_bins.spec.js
@@ -0,0 +1,23 @@
+import {categorical_bins} from "apps/irs_monitor/lib/bin_responses"
+
+describe('categorical bins', () => {
+
+  it('bins responses based on a string in options.bin_by', () => {
+    const responses = [
+      {type: 1},
+      {type: 2},
+      {type: 1}
+    ]
+    const options = {
+      bin_by: 'type'
+    }
+    const result = categorical_bins({responses, options})
+
+    assert.lengthOf(result, 2)
+    assert.lengthOf(result[0].values, 2)
+
+    assert.equal(result[0].key, 1)
+    assert.equal(result[1].key, 2)
+  })
+
+})
