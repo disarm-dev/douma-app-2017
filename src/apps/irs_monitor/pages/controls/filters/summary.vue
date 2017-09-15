de4d44c7ea1c8d46ab85200a54154a216ccb749c
Nicolai Davies <nicolaidavies.nd@gmail.com>
2017-09-15 14:59:11 +0200

    A button. But the tests pass without it?

--- src/apps/irs_monitor/pages/controls/filters/summary.vue
+++ src/apps/irs_monitor/pages/controls/filters/summary.vue
@@ -1,28 +1,21 @@
 <template>
     <div>
         <h2>Active filters</h2>
-        <md-chip v-for="({name, comparator, value}, index) in filters" :key="index">
+        <md-chip v-for="({name, comparator, value}, index) in filters" :key="index" md-deletable @delete="on_delete(filter)">
             {{name}} {{comparator}} {{value}}
         </md-chip>
     </div>
 </template>
 
 <script>
-  //import { mapState, mapActions, mapMutations } from 'vuex'
-
   export default {
     name: 'summary',
-    data() {
-      return {
-        filters: [
-          {name: 'name', comparator: 'eq', value: 'me'},
-          {name: 'name2', comparator: 'eq', value: 'me2'}
-        ]
+    props: ['filters'],
+    methods: {
+      on_delete(filter) {
+        this.$emit('remove_filter', filter)
       }
-    },
-    mounted() {
-    },
-    methods: {}
+    }
   }
 </script>
 
