24d9ba89efe0296beef5eb42d9090c58425042d0
Jonathan Smith <jonathan@peoplesized.com>
2017-09-18 16:51:11 +0200

    Revert "Revert "Merge branch 'feature/instance-specific-fixes' into 'develop'""
    This reverts commit 0adb47e701ee50329077d18731f6f7e3a8d3e7e4.

--- src/config/common.js
+++ src/config/common.js
@@ -31,7 +31,7 @@ export default {
     // Meta below is currently ignored in sidebar (statically included), but here for the breadcrumbs
     'meta': {title: 'User', icon: 'person'},
   },
-  temporal_intervals: ['week', 'month', 'quarter'],
+  temporal_intervals: ['week', 'month', 'quarter', 'year'],
   basemap: {
     // Middle of southern Africa, start point for zooming in
     default: {
