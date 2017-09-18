53e8fe19592310fdba14b2418e4b10e451b18cd6
Nicolai Davies <nicolaidavies.nd@gmail.com>
2017-09-18 15:51:39 +0200

    Initial remote and local response tests

---
+++ test/unit/specs/models/responses/remote.spec.js
@@ -0,0 +1,22 @@
+import {get_all_records} from "lib/models/response/remote";
+
+xdescribe('Responses remote', () => {
+
+  describe('Get records', () => {
+    it('should return a promise', () => {
+
+    })
+
+    it('should return a promise resolving with an array of objects', () => {
+
+    })
+  })
+
+  describe('Create records', () => {
+    it('should throw an error if no records are passed in', () => {
+
+    })
+
+
+  })
+})
