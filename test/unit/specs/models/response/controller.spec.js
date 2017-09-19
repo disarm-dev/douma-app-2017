4209ac6332fba25bdba040529c3bf69203dc7c7a
Jonathan Smith <jonathan@peoplesized.com>
2017-09-19 14:11:44 +0200

    comments

--- test/unit/specs/models/response/controller.spec.js
+++ test/unit/specs/models/response/controller.spec.js
@@ -16,19 +16,13 @@ describe('responses.controller', () => {
 
 
 
-// responses/local.js
-  // create
-  // (read one) - we get single records from $store
-  // read all
-  // update
-  // (delete) - not currently used, closely linked to `update`
+  // SIMPLE (proxied to responses.local.js)
+  // * create - make new record from RECORD
+  // * read all - view them all in MONITOR
+  // * update - edit in RECORD
+  // (delete) - not yet
 
-// responses/remote.js
-  // create
-  // (read one)
-  // read all
-  // (update)
-  // (delete)
+
 
 // responses/controller.js
 // calls to these functions come directly from the $store -
