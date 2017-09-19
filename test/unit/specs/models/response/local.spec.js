a9e72727bb735360aa63a065218b29b8bcde9807
Jonathan Smith <jonathan@peoplesized.com>
2017-09-19 14:11:33 +0200

    refac test to use only equal!±

--- test/unit/specs/models/response/local.spec.js
+++ test/unit/specs/models/response/local.spec.js
@@ -2,16 +2,19 @@ import local from 'lib/models/response/local'
 
 describe('Responses local', () => {
   describe('basic CRUD functions exist', () => {
-    // SIMPLE (proxied to responses.local.js)
-    // * create - make new record from RECORD
-    // * read all - view them all in MONITOR
-    // * update - edit in RECORD
-    // (delete) - not yet
+    // responses/local.js
+    // create
+    // (read one) - we get single records from $store
+    // read all
+    // update
+    // (delete) - not currently used, closely linked to `update`
 
-    it('create, read all and update all exist as functions', () => {
-      assert.isFunction(local.create)
-      assert.isFunction(local.read_all)
-      assert.isFunction(local.update)
+    it('create, read_all and update all exist as functions', () => {
+      const expected = 'function'
+
+      assert.equal(typeof local.create, expected)
+      assert.equal(typeof local.read_all, expected)
+      assert.equal(typeof local.update, expected)
     })
 
   })
