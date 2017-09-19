440907c28ff36a2a78664098e254a13d8627a542
Jonathan Smith <jonathan@peoplesized.com>
2017-09-19 13:54:55 +0200

    another comment

--- test/unit/specs/controllers/responses.controller.spec.js
+++ test/unit/specs/controllers/responses.controller.spec.js
@@ -3,21 +3,6 @@ describe('responses.controller', () => {
 
   })
 
-// remote get all
-// get 'are there any remote updated'
-// store last-checked time / hash
-// decorate
-// save to local
-// create new locally
-// sync all to remote
-// delete locally
-// edit locally
-// clear all local
-// refresh from remote
-//
-
-
-
 // responses.local.js
   // create
   // (read one) - we get single records from $store
@@ -38,7 +23,7 @@ describe('responses.controller', () => {
 
   // WOVEN TOGETHER
   // * "SYNC NEW RECORDS": get all new local records, write to remote (e.g. for RECORD)
-  // * "REFRESH RECORDS": get all remote records, write to local (e.g. for MONITOR)
+  // * "REFRESH RECORDS": get all remote records, decorate, write to local (e.g. for MONITOR)
   // (sync - i.e. find difference in state between local+remote and rectify)
 
   // SIMPLE (proxied to responses.local.js)
