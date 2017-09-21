77aa85bb9b452813c51afbad35a75f6508370542
Nicolai Davies <nicolaidavies.nd@gmail.com>
2017-09-21 14:09:49 +0200

    Test need_to_update. It depends on standard handler and on other action. Good example of how to handle responses, plans an other.

--- test/unit/specs/root_store.spec.js
+++ test/unit/specs/root_store.spec.js
@@ -48,4 +48,58 @@ describe('root store', () => {
     })
 
   })
+
+  describe('need_to_update', () => {
+
+    it("should return can update if versions don't match", (done) => {
+      const instance_config = {}
+      const instance_stores = {}
+      const store = create_store(instance_config, instance_stores)
+
+      const callback = sinon.spy()
+
+      store.dispatch('need_to_update').then(callback)
+
+      moxios.wait(function () {
+        let request = moxios.requests.mostRecent()
+        request.respondWith({
+          status: 200,
+          response: 'not_real_version'
+        }).then(function () {
+          const expected = {status: 'CAN_UPDATE', local_version: VERSION_COMMIT_HASH_SHORT, remote_version: 'not_real_version'}
+
+          assert.equal(callback.called, true)
+          assert.deepEqual(expected, callback.getCall(0).args[0])
+
+          done()
+        })
+      })
+    })
+
+    it("should return can update if versions don't match", (done) => {
+      const instance_config = {}
+      const instance_stores = {}
+      const store = create_store(instance_config, instance_stores)
+
+      const callback = sinon.spy()
+
+      store.dispatch('need_to_update').then(callback)
+
+      moxios.wait(function () {
+        let request = moxios.requests.mostRecent()
+        request.respondWith({
+          status: 200,
+          response: VERSION_COMMIT_HASH_SHORT
+        }).then(function () {
+          const expected = {status: 'ON_LATEST', local_version: VERSION_COMMIT_HASH_SHORT, remote_version: VERSION_COMMIT_HASH_SHORT}
+
+          assert.equal(callback.called, true)
+          assert.deepEqual(expected, callback.getCall(0).args[0])
+
+          done()
+        })
+      })
+    })
+
+  })
 })
