66344da518e438cbdc4cd93b54b5817bbf103131
Jonathan Smith <jonathan@peoplesized.com>
2017-09-25 12:36:46 +0200

    removed unused (and inappropriate) test

--- test/specs/components/irs_monitor/chart.test.js
+++ test/specs/components/irs_monitor/chart.test.js
@@ -3,16 +3,6 @@ import {shallow} from 'vue-test-utils'
 
 import Chart from 'apps/irs_monitor/pages/charts/chart.vue'
 
-
-// xit('try without a chart_type and throw an error', () => {
-//   const Ctor = Vue.extend(Chart)
-//   const fn = new Ctor().$mount
-//   const vm = fn()
-//   console.log('vm', vm)
-//
-//   assert.throws(fn, 'Missing `options.chart_type`')
-// })
-
 test('can create component', t => {
   const wrapper = shallow(Chart)
   t.true(wrapper.exists())
