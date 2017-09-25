14b2f53fab44f6db47360b3eaa04579ad0e4be59
Jonathan Smith <jonathan@peoplesized.com>
2017-09-25 10:55:20 +0200

    actually close #188 - add defaults for globals when not created by webpack

--- src/config/build-time.js
+++ src/config/build-time.js
@@ -1,3 +1,11 @@
+// See `webpack.base.conf.js`
+
+// Set some defaults for testing, and anything else that doesn't use webpack
+const __VERSION_COMMIT_HASH_SHORT = 'DEFAULT FOR TESTING'
+const __BRANCH = 'DEFAULT FOR TESTING'
+const __DOUMA_PRODUCTION_MODE = true
+const __GA_ANALYTICS_UA = ''
+
 export default {
   VERSION_COMMIT_HASH_SHORT: __VERSION_COMMIT_HASH_SHORT,
   BRANCH: __BRANCH,
