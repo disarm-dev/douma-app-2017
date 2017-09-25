3f24ec64374761f2078e4d2c5ce6d2f6617fa6c3
Jonathan Smith <jonathan@peoplesized.com>
2017-09-25 22:37:17 +0200

    stub of response remote test

--- test/specs/models/response/controller.test.js
+++
@@ -1,7 +0,0 @@
-// The controllers are the `index.js` files for each model
-
-import test from 'ava'
-
-import {read_all_network} from 'lib/models/response'
-
-test('', t => {})
---
+++ test/specs/models/response/remote.test.js
@@ -0,0 +1,36 @@
+import test from 'ava'
+import moxios from 'moxios'
+
+import remote from 'lib/models/response/remote'
+import {instantiate_axios_instance} from 'lib/remote/axios_instance'
+
+test.beforeEach(() => {
+  instantiate_axios_instance()
+  moxios.install()
+})
+
+test.afterEach(() => {
+  moxios.uninstall()
+})
+
+
+test.cb('test test', t => {
+  remote.read_all().then(() => {
+    moxios.wait(function () {
+      let request = moxios.requests.mostRecent()
+      request.respondWith({
+        status: 200,
+        response: [
+          { id: 1, firstName: 'Fred', lastName: 'Flintstone' },
+          { id: 2, firstName: 'Wilma', lastName: 'Flintstone' }
+        ]
+      }).then(function () {
+        t.end()
+      })
+    })
+  }).catch(() => {
+    t.fail('caught error')
+    t.end()
+  })
+
+})
