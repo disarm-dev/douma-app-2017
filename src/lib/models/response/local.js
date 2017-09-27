5a2b2199e99a84763d0e5327724afb9528fc2c20
Nicolai Davies <nicolaidavies.nd@gmail.com>
2017-09-27 10:35:14 +0200

    Rename to table to match dexie docs

--- src/lib/models/response/local.js
+++ src/lib/models/response/local.js
@@ -2,18 +2,18 @@ import {db} from 'lib/local_db'
 
 export default class Local {
   constructor(applet_name) {
-    this.collection = db[applet_name + '/responses']
+    this.table = db[applet_name + '/responses']
   }
 
   async read_all() {
-    return await this.collection.toArray()
+    return await this.table.toArray()
   }
 
   async create(response) {
-    await this.collection.add(response)
+    await this.table.add(response)
   }
 
   async bulk_create(responses) {
-    await this.collection.bulkAdd(responses)
+    await this.table.bulkAdd(responses)
   }
 }
