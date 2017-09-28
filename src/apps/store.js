f5293dab61adbc1fdf93b21a7bf72517dda1fd0c
Jonathan Smith <jonathan@peoplesized.com>
2017-09-28 10:53:05 +0200

    extract creation of options for vuex-persistedstate

--- src/apps/store.js
+++ src/apps/store.js
@@ -1,69 +1,30 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+
 import createPersistedState from 'vuex-persistedstate'
 import { createVuexLoader } from 'vuex-loading'
-import {get, set, cloneDeep} from 'lodash'
 
-let store
+import {generate_persisted_state_options} from 'config/persisted_state_options'
 import CONFIG from 'config/common'
 
+let store
 export {store}
 
 export function create_store(instance_config, instance_stores) {
-  // Theses excluded paths are for the root store
-  let unpersisted_state = [{store_path: 'sw_update_available', default_value: false}, {store_path: 'sw_message', default_value: {message: null, title: null}}]
-
-  for (const store_name in instance_stores) {
-    const store = instance_stores[store_name]
-    const paths = get(store, 'unpersisted_state_keys', [])
-
-    paths.forEach(path => {
-      const store_path = `${store_name}.${path}`
-      const default_value = get(store.state, path, 'eggs')
-      unpersisted_state.push({store_path, default_value})
-    })
-  }
-  
+  // use vuex
   Vue.use(Vuex)
-
-  // vuex-persistedstate
-  const persisted_state_options = {
-    getState:(key, storage) => {
-      const value = storage.getItem(key);
-
-      try {
-        return value && value !== 'undefined' ? JSON.parse(value) : undefined;
-      } catch (err) {
-        return undefined;
-      }
-    },
-    setState: (key, state, storage) => {
-      setTimeout(() => storage.setItem(key, JSON.stringify(state)), 0)
-    },
-    reducer: (state) => {
-      if (unpersisted_state.length === 0) {
-        return state
-      } else {
-        const state_copy = cloneDeep({}, state)
-
-        unpersisted_state.forEach(function({store_path, default_value}) {
-          set(state_copy, store_path, default_value)
-        })
-
-        return state_copy
-      }
-    }
-  }
 
   // vuex-loader
   const VuexLoading = createVuexLoader(CONFIG.vuex_loader_options)
   Vue.use(VuexLoading)
 
+  // Generate config (reducer, etc) for vuex-persistedstate
+  // This includes the paths for unpersisted state for large data objects
+  const persisted_state_options = generate_persisted_state_options(instance_stores)
 
   store = new Vuex.Store({
     modules: instance_stores,
     plugins: [createPersistedState(persisted_state_options), VuexLoading.Store],
-    // plugins: [VuexLoading.Store],
     state: {
       // Global config
       instance_config: instance_config, // Really important, should maybe be somewhere else
