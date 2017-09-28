992aadff4c5e3dfe6ef5dbdb9de0859f25c22089
Nicolai Davies <nicolaidavies.nd@gmail.com>
2017-09-28 11:27:54 +0200

    Split out tests, so it easier to see which part of read_all_network is failing

--- test/specs/models/response/controller.test.js
+++ test/specs/models/response/controller.test.js
@@ -3,35 +3,55 @@ import sinon from 'sinon'
 
 import {ResponseController} from "lib/models/response/controller"
 
-test("can be instantiated", t => {
-  const controller = new ResponseController('test')
-  t.true(controller instanceof ResponseController)
-})
-
-test.cb("calls remote and local methods in read_all_network and returns result", t => {
-
+function setup() {
   const responses = [{id: 1}, {id: 2}]
 
   const read_all = sinon.stub().returns(Promise.resolve(responses))
-  const remote = { read_all }
+  const remote = {read_all}
 
   const create_bulk = sinon.stub().returns(Promise.resolve())
-  const local = { create_bulk }
+  const local = {create_bulk}
 
   const controller = new ResponseController('test')
 
   controller.local = local
   controller.remote = remote
 
+  return {responses, controller}
+}
 
-  controller.read_all_network().then((actual) => {
+test("can be instantiated", t => {
+  const controller = new ResponseController('test')
+  t.true(controller instanceof ResponseController)
+})
+
+test.cb("calls remote methods in read_all_network ", t => {
+  const {controller} = setup()
+
+  controller.read_all_network().then(() => {
     t.true(controller.remote.read_all.calledOnce)
-    t.true(controller.local.create_bulk.calledOnce)
+    t.end()
+  })
+})
 
+test.cb("calls local methods in read_all_network with correct arguments", t => {
+  const {responses, controller} = setup()
+
+  controller.read_all_network().then(() => {
+    t.true(controller.local.create_bulk.calledOnce)
     t.deepEqual(responses, controller.local.create_bulk.getCall(0).args[0])
-    t.deepEqual(responses, actual)
 
     t.end()
   })
 })
+
+test.cb("read_all_network returns responses", t => {
+  const {responses, controller} = setup()
+
+  controller.read_all_network().then((actual) => {
+    t.deepEqual(responses, actual)
+    t.end()
+  })
+})
+
 
