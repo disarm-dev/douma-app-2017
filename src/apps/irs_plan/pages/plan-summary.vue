e81d74934ec55e0dbf98c75942e091c49d82b6c0
Jonathan Smith <jonathan@peoplesized.com>
2017-09-29 13:34:54 +0200

    change button text to "Download plan table"

--- src/apps/irs_plan/pages/plan-summary.vue
+++ src/apps/irs_plan/pages/plan-summary.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h3>Selected {{planning_level_name}}:</h3>
-    <md-button class='md-raised md-primary' @click.native="download_plan">Download plan</md-button>
+    <md-button class='md-raised md-primary' @click.native="download_plan">Download plan table</md-button>
     <v-client-table
       v-if="selected_target_area_ids.length !== 0"
       :data="table.data"
