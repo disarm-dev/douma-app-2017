0ee25f88dc5b93e99589a0db031212f5d799d7d9
Jonathan Smith <jonathan@peoplesized.com>
2017-09-29 08:36:18 +0200

    more polite log

--- src/apps/store.js
+++ src/apps/store.js
@@ -26,7 +26,7 @@ export function create_store(instance_config, instance_stores) {
       }
     },
     setState: (key, state, storage) => {
-      console.warn("👮‍ You're doing something dumb. (setting-and-forgetting localStorage with no checks)")
+      console.warn("👮‍ stringifying whole store into localStorage")
       setTimeout(() => storage.setItem(key, JSON.stringify(state)), 0)
     },
     reducer: (state) => {
