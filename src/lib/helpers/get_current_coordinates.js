1d130880e6ef246c3aa33da0d9ac6597a4ac320e
Nicolai Davies <nicolaidavies.nd@gmail.com>
2017-10-26 09:31:53 +0200

    refactor

--- src/lib/helpers/get_current_coordinates.js
+++ src/lib/helpers/get_current_coordinates.js
@@ -1,4 +1,4 @@
-function watch_n_positions(duration_ms , options) {
+function watch_positions_for(duration_ms , options) {
   return new Promise((resolve) => {
     let watch_id
     const result = {
@@ -12,11 +12,9 @@ function watch_n_positions(duration_ms , options) {
     }, duration_ms)
 
     watch_id = navigator.geolocation.watchPosition((position) => {
-      console.log('position from watchPosition', position)
       result.positions.push(position)
     },
     (error) => {
-      console.log('error from watchPosition', error)
       result.errors.push(error)
     },
     options
@@ -24,10 +22,20 @@ function watch_n_positions(duration_ms , options) {
   })
 }
 
+export async function get_current_coordinates() {
+  const options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 2000
+  }
+
+  const {errors, positions} = await watch_positions_for(5000, options)
+  return determine_response(positions, errors)
+}
+
 export async function determine_response(positions, errors) {
   if (positions.length > 0) {
     // If we have more than one position then return the position with the lowest accuracy value (the most accurate position).
-    console.log('positions', positions)
     const position_with_highest_accuracy = positions.reduce((highest_accuracy, position) => {
       if (position.coords.accuracy < highest_accuracy.coords.accuracy) {
         highest_accuracy = position
@@ -41,17 +49,4 @@ export async function determine_response(positions, errors) {
     const latest_error = errors[errors.length - 1]
     throw latest_error
   }
-}
-
-export async function get_current_coordinates() {
-  const options = {
-    enableHighAccuracy: true,
-    timeout: 5000,
-    maximumAge: 2000
-  }
-
-  const {errors, positions} = await watch_n_positions(5000, options)
-
-
-  return determine_response(positions, errors)
 }
