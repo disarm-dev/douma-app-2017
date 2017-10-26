5f3f517d779db4d93c55260303e13cbcf028e3d4
Nicolai Davies <nicolaidavies.nd@gmail.com>
2017-10-26 09:40:59 +0200

    Fix failing test and test formatting of dates

--- test/specs/components/irs_monitor/filter_summary.test.js
+++ test/specs/components/irs_monitor/filter_summary.test.js
@@ -10,8 +10,9 @@ const filters = [
 ]
 
 test('should accept filters as a prop', t => {
-  const wrapper = mount(Summary)
-  wrapper.setProps({filters})
+  const wrapper = mount(Summary, {
+    propsData: {filters}
+  })
 
   t.true(wrapper.hasProp('filters', filters))
 })
@@ -29,4 +30,18 @@ test('should emit remove_filter event when removing a filter', t => {
 
   t.deepEqual(wrapper.vm.$emit.getCall(0).args[0], 'remove_filter')
   t.deepEqual(wrapper.vm.$emit.getCall(0).args[1], filters[0])
+})
+
+
+test('should format a recorded_on filter', t => {
+  const filters = [{name: 'recorded_on', comparator: 'eq', value: 'Thu Oct 26 2017 09:37:17 GMT+0200 (SAST)'}]
+
+  const wrapper = shallow(Summary, {
+    propsData: {filters}
+  })
+
+  const actual = wrapper.vm.formatted_filters[0]
+  const expected = {name: 'recorded_on', comparator: 'eq', value: 'Oct 26th 2017'}
+
+  t.deepEqual(actual, expected)
 })
