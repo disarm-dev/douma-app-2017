a665d0de1e54d3236cf3f81764e096e61de12a0f
Nicolai Davies <nicolaidavies.nd@gmail.com>
2017-10-26 10:00:54 +0200

    Formatting.

--- src/lib/helpers/get_current_coordinates.js
+++ src/lib/helpers/get_current_coordinates.js
@@ -1,4 +1,4 @@
-function watch_positions_for(duration_ms , options) {
+function watch_positions_for(duration_ms, options) {
   return new Promise((resolve) => {
     let watch_id
     const result = {
@@ -12,12 +12,12 @@ function watch_positions_for(duration_ms , options) {
     }, duration_ms)
 
     watch_id = navigator.geolocation.watchPosition((position) => {
-      result.positions.push(position)
-    },
-    (error) => {
-      result.errors.push(error)
-    },
-    options
+        result.positions.push(position)
+      },
+      (error) => {
+        result.errors.push(error)
+      },
+      options
     )
   })
 }
@@ -33,20 +33,22 @@ export async function get_current_coordinates() {
   return determine_response(positions, errors)
 }
 
-export async function determine_response(positions, errors) {
-  if (positions.length > 0) {
-    // If we have more than one position then return the position with the lowest accuracy value (the most accurate position).
-    const position_with_highest_accuracy = positions.reduce((highest_accuracy, position) => {
-      if (position.coords.accuracy < highest_accuracy.coords.accuracy) {
-        highest_accuracy = position
-      }
-      return highest_accuracy
-    })
+export function determine_response(positions, errors) {
+  return new Promise((resolve, reject) => {
+    if (positions.length > 0) {
+      // If we have more than one position then return the position with the lowest accuracy value (the most accurate position).
+      const position_with_highest_accuracy = positions.reduce((highest_accuracy, position) => {
+        if (position.coords.accuracy < highest_accuracy.coords.accuracy) {
+          highest_accuracy = position
+        }
+        return highest_accuracy
+      })
 
-    return position_with_highest_accuracy
-  } else {
-    // Else throw the latest error.
-    const latest_error = errors[errors.length - 1]
-    throw latest_error
-  }
+      resolve(position_with_highest_accuracy)
+    } else {
+      // Else throw the latest error.
+      const latest_error = errors[errors.length - 1]
+      reject(latest_error)
+    }
+  })
 }
