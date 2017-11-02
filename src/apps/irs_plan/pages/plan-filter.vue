92dd67eecd270139ec6af264ba27c27a961ebb3b
Jonathan Smith <jonathan@peoplesized.com>
2017-11-02 20:30:17 +0200

    Revert "try out removing disabled focus-fitler-area selection - was causing problem with reloaded pages always being disabled."
    This reverts commit 3dc88f46f1327bd3bb4026083be4bb53033020ad.

--- src/apps/irs_plan/pages/plan-filter.vue
+++ src/apps/irs_plan/pages/plan-filter.vue
@@ -4,6 +4,8 @@
       <h4>Select area to focus plan on</h4>
       <p>With an area selected, you won't be able to edit or save anywhere else.</p>
       <multiselect
+        :disabled="unsaved_changes"
+
         :value="selected_filter_area_option"
         @select="select_filter"
         :options="filter_options"
@@ -33,6 +35,7 @@
   export default {
     name: 'plan-filter',
     components: {Multiselect},
+    props: ['unsaved_changes'],
     mounted() {
     },
     computed: {
