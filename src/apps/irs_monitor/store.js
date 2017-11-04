f89d7ed8eabf80426048a9ce0bc99645b828f74e
Jonathan Smith <jonathan@peoplesized.com>
2017-11-04 16:10:36 +0200

    Plan().validate doesn't return, just throws if it gets any errors

--- src/apps/irs_monitor/store.js
+++ src/apps/irs_monitor/store.js
@@ -144,8 +144,11 @@ export default {
       return plan_controller.read_plan_current_network()
         .then(plan_json => {
           if (Object.keys(plan_json).length === 0) throw {message: 'No plan created. Please create one.'}
-          if (!new Plan().validate(plan_json)) throw {message: 'Plan is not valid'}
-          context.commit('set_plan', plan_json)
+          if (new Plan().validate(plan_json)) {
+            context.commit('set_plan', plan_json)
+          } else {
+            console.error('plan_json is not a Plan', plan_json)
+          }
         })
     }
   }
