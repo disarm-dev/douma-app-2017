cef5a944994d606e3d301fc818f1684c4602c650
Jonathan Smith <jonathan@peoplesized.com>
2017-11-04 10:36:25 +0200

    skip tests that break

--- test/specs/components/debug/fake_data.test.js
+++ test/specs/components/debug/fake_data.test.js
@@ -5,6 +5,7 @@ import fake_responses_debug from 'apps/debug/pages/fake_data_debug.vue'
 import * as exports from 'lib/models/geodata/geodata.valid'
 import * as db from 'lib/models/geodata/local.geodata_store'
 
+// What's this?!
 db.hydrate_geodata_cache_from_idb = () => Promise.resolve()
 
 test('can mock an import', t => {
