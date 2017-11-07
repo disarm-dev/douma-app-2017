96c4a866a2520d03aed024dd7f8ebf332f9385bf
Jonathan Smith <jonathan@peoplesized.com>
2017-11-07 11:20:19 +0200

    renamed

--- src/lib/remote/request-handler.js
+++ src/lib/remote/request-handler.js
@@ -6,27 +6,27 @@ import {store} from 'apps/store'
 /**
  * Standard request handler for all remote requests (currently both client server and API)
  * Passed options overwrite any default options.
- * @param request
+ * @param request_config
  */
-export function request_handler(request) {
+export function request_handler(request_config) {
 
-  if (!request) return Promise.reject(new Error("request is empty"))
+  if (!request_config) return Promise.reject(new Error("request is empty"))
 
   // If a `request.url` is not already provided, will create one
   // to send request to API server
-  if (!request.url) {
-    if (!request.url_suffix && request.url_suffix !== '') throw new Error("Missing `url_suffix` on request")
+  if (!request_config.url) {
+    if (!request_config.url_suffix && request_config.url_suffix !== '') throw new Error("Missing `url_suffix` on request")
 
     // Get API URL - either from localStorage or the default from CONFIG
     const api_url = get_api_url()
 
     const douma_api_root = `${api_url}/${CONFIG.api.version}`
-    request.url = douma_api_root + request.url_suffix
+    request_config.url = douma_api_root + request_config.url_suffix
   }
 
   const axios_instance = config_axios_instance()
 
-  return axios_instance(request)
+  return axios_instance(request_config)
     .then(json => json.data)
     .catch(err => {
       // Any route other than login which receives 401 needs to tell user
