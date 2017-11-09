e6bf289667fe2b266bc3072ab3884fc916b50fed
Nicolai Davies <nicolaidavies.nd@gmail.com>
2017-11-09 13:40:12 +0200

    Unique list

--- src/lib/models/user/permission_helper.js
+++ src/lib/models/user/permission_helper.js
@@ -1,3 +1,5 @@
+import unique from 'array-unique'
+
 /**
  * Checks if next applet is included in user's applets with 'read' permission
  * @param user
@@ -7,5 +9,6 @@ export const has_permission = ({user, applet_name}) => {
   if (['meta'].includes(applet_name)) return true
 
   const list_of_applets = user.permissions.map(p => p.replace('write:', '').replace('read:', ''))
-  return list_of_applets.includes(applet_name)
+  const user_allowed_applets = unique(list_of_applets)
+  return user_allowed_applets.includes(applet_name)
 }
