5edcba80e8ba18dea4c91c525e52c8cc189a8417
Nicolai Davies <nicolaidavies.nd@gmail.com>
2017-11-30 11:48:31 +0200

    There is no 'production' branch, switch to master

--- build/analytics_ua_for_branch.js
+++ build/analytics_ua_for_branch.js
@@ -3,7 +3,7 @@ const staging_ua = 'UA-88844641-3'
 
 module.exports = (gitRevisionPlugin) => {
   const branch = JSON.stringify(gitRevisionPlugin.branch())
-  if (branch === 'production') {
+  if (branch === 'master') {
     return production_ua
   } else {
     return staging_ua
