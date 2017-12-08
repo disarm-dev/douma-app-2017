b11decb49c94edc3862d5bfe7663bf0bfeae4e45
Jonathan Smith <jonathan@peoplesized.com>
2017-12-08 13:30:05 +0200

    JSON.stringify wraps branch in quotes: not needed, and doesn't compare well to a string...

--- build/analytics_ua_for_branch.js
+++ build/analytics_ua_for_branch.js
@@ -2,8 +2,8 @@ const production_ua = 'UA-88844641-2'
 const staging_ua = 'UA-88844641-3'
 
 module.exports = (gitRevisionPlugin) => {
-  const branch = JSON.stringify(gitRevisionPlugin.branch())
-  if (branch === 'master') {
+  const branch = gitRevisionPlugin.branch()
+  if (branch == 'master') {
     return production_ua
   } else {
     return staging_ua
