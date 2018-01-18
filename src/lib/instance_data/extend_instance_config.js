e100f6dbf7a967b8a73a6ae07ffe3fb99aaf35b1
Nicolai Davies <nicolaidavies.nd@gmail.com>
2018-01-18 11:01:09 +0100

    Correct path for config

--- src/lib/instance_data/extend_instance_config.js
+++ src/lib/instance_data/extend_instance_config.js
@@ -6,12 +6,13 @@ import CONFIG from 'config/common'
 // Instance configuration and related files
 export const get_instance_files = async (slug) => {
   const res = await fetch(`https://application-registry-server.herokuapp.com/api/config/${slug}`)
-  const instance_json = await res.json()
+  const json = await res.json()
+  const instance = json.config_data
 
   // TODO: @refac This should probably live somewhere else
   // A standard 'count' aggregation is needed for everything
-  instance_json.aggregations.push({name: 'count', numerator_expr: '1'})
+  instance.aggregations.push({name: 'count', numerator_expr: '1'})
 
-  return instance_json
+  return instance
 }
 
