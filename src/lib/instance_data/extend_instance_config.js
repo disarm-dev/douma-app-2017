8f38b3f7f278ce1115ece0d6163d8a1a630ece75
Nicolai Davies <nicolaidavies.nd@gmail.com>
2018-01-18 10:58:25 +0100

    Believe this should be enough, not tested!

--- src/lib/instance_data/extend_instance_config.js
+++ src/lib/instance_data/extend_instance_config.js
@@ -4,42 +4,14 @@ import CONFIG from 'config/common'
 
 
 // Instance configuration and related files
-export const get_instance_files = (slug) => {
-  const types = CONFIG.instances.required_instance_files
+export const get_instance_files = async (slug) => {
+  const res = await fetch(`https://application-registry-server.herokuapp.com/api/config/${slug}`)
+  const instance_json = await res.json()
 
-  return Promise.all(types.map(type => get_instance_file(slug, type)))
-    .then(jsons => {
-      let instance_config = jsons[0]
-      // Remove `instance` from the incoming files, it is the root to which the others are added.
-      jsons.splice(0, 1)
-      types.splice(0, 1)
+  // TODO: @refac This should probably live somewhere else
+  // A standard 'count' aggregation is needed for everything
+  instance_json.aggregations.push({name: 'count', numerator_expr: '1'})
 
-      const errors = IncomingInstanceConfigSchema.errors(instance_config)
-
-      if (errors) {
-        const message = "Invalid instance_config"
-        alert(message + ": If this happens when you reload, please report as an urgent bug! Thanks")
-        throw new Error(`${message} ${JSON.stringify(errors)}`)
-      }
-
-      // Create object to match up the list of types with the retrieved data
-      // Structure of the `jsons_object` is determined by CONFIG.instances.required_instance_files
-      const jsons_object = types.reduce((acc, type, index) => {
-        acc[type] = jsons[index]
-        return acc
-      }, {})
-
-      // Other elements to attach
-      const instance = {
-        ...instance_config,
-        ...jsons_object
-      }
-      // TODO: @refac This should probably live somewhere else
-      // A standard 'count' aggregation is needed for everything
-      instance.aggregations.push({name: 'count', numerator_expr: '1'})
-
-      return instance
-    })
-
+  return instance_json
 }
 
